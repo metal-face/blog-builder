a95d536440593af8c6533827cf0cbc70cc10ac70
metal-face <hughesbryan3000@gmail.com>
2023-12-28 03:42:29 -0500

    clock started but not correct

--- components/clock.tsx
+++ components/clock.tsx
@@ -1,10 +1,20 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { TypographyH2 } from "./typography/typography-h2";
+
 export function Clock() {
+  function updateTime() {
+    setTime(new Date());
+  }
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    setInterval(updateTime, 1000);
+  }, []);
+
   return (
-    <div className="flex items-center justify-center">
-      <TypographyH2 props={{ text: "11:59pm" }}></TypographyH2>
+    <div className="m-2 flex items-center justify-center">
+      <TypographyH2 props={{ text: time.toLocaleTimeString() }} />
     </div>
   );
 }
