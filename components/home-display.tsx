1c8eb6e5430504dd95ab0a6767b7f285b52b16aa
metal-face <hughesbryan3000@gmail.com>
2023-12-28 03:20:48 -0500

    Add Clock component to home display

--- components/home-display.tsx
+++ components/home-display.tsx
@@ -1,8 +1,10 @@
 import { TypeographyH1 } from "@/components/typography/typography-h1";
+import { Clock } from "@/components/clock";
 export function HomeDisplay() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
       <TypeographyH1 props={{ text: "Good Day" }}></TypeographyH1>
+      <Clock />
     </div>
   );
 }
