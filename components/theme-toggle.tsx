83e6f12af8bb2d06bbf65de272aaad66f98d7242
metal-face <hughesbryan3000@gmail.com>
2023-12-28 00:58:43 -0500

    Add theme toggle component

---
+++ components/theme-toggle.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import * as React from "react";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
