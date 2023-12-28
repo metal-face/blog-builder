7d915550991781e59b8bb61d9a3afb76dd47fde5
metal-face <hughesbryan3000@gmail.com>
2023-12-28 01:09:56 -0500

    light/dark mode toggle added

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import * as React from "react";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export function MainNav() {
   return (
     <div className="h-12 border flex items-center justify-end">
-      
+      <ThemeToggle></ThemeToggle>
       <button className="text-pretty m-4">Login</button>
     </div>
   );
