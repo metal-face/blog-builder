766f7412dc9617f744122f2983034440f3733093
metal-face <hughesbryan3000@gmail.com>
2023-12-28 01:13:44 -0500

    Add GitHub login button to main navigation

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -2,12 +2,17 @@
 
 import * as React from "react";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export function MainNav() {
   return (
     <div className="h-12 border flex items-center justify-end">
       <ThemeToggle></ThemeToggle>
-      <button className="text-pretty m-4">Login</button>
+      <Button variant="outline" className="m-2">
+        Login
+        <GitHubLogoIcon className="h-4 w-4 m-2" />
+      </Button>
     </div>
   );
 }
