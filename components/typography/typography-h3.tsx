a0a6b771365c88ebba3f3cde013b73e846e9cfa7
metal-face <hughesbryan3000@gmail.com>
2023-12-29 01:49:04 -0500

    Refactor TypographyH3 component to destructure props

--- components/typography/typography-h3.tsx
+++ components/typography/typography-h3.tsx
@@ -1,14 +1,12 @@
 "use client";
 interface TypographyH3Props {
-  props: {
-    text: string;
-  };
+  text: string;
 }
 
-export default function TypographyH3({ props }: TypographyH3Props) {
+export default function TypographyH3({ text }: TypographyH3Props) {
   return (
     <h3 className="m-1 scroll-m-20 text-2xl font-semibold tracking-tight">
-      {props.text}
+      {text}
     </h3>
   );
 }
