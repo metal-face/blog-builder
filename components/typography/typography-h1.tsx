72b35a2ee5909657eaa03ec049da9a8f9e489b17
metal-face <hughesbryan3000@gmail.com>
2023-12-28 22:26:43 -0500

    Refactor typography-h1 component props and styling

--- components/typography/typography-h1.tsx
+++ components/typography/typography-h1.tsx
@@ -1,12 +1,12 @@
-interface TypeographyH1Props {
+interface TypographyH1Props {
   props: {
     text: string;
   };
 }
 
-export function TypeographyH1({ props }: TypeographyH1Props) {
+export function TypographyH1({ props }: TypographyH1Props) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1 className="m-1 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
       {props.text}
     </h1>
   );
