8bff1c60a7f8e5aeb552cfdea328f3139798da49
metal-face <hughesbryan3000@gmail.com>
2023-12-29 22:41:16 -0500

    Add ForwardRefEditor component

---
+++ components/forward-ref-editor.tsx
@@ -0,0 +1,21 @@
+"use client";
+// ForwardRefEditor.tsx
+
+// This is the only place InitializedMDXEditor is imported directly.
+import dynamic from "next/dynamic";
+import { forwardRef } from "react";
+import { MDXEditorMethods, MDXEditorProps } from "@mdxeditor/editor";
+
+const Editor = dynamic(() => import("./initialized-mdx-editor"), {
+    // Make sure we turn SSR off
+    ssr: false,
+});
+
+// This is what is imported by other components. Pre-initialized with plugins, and ready
+// to accept other props, including a ref.
+export const ForwardRefEditor = forwardRef<MDXEditorMethods, MDXEditorProps>(
+    (props, ref) => <Editor {...props} editorRef={ref} />
+);
+
+// TS complains without the following line
+ForwardRefEditor.displayName = "ForwardRefEditor";
