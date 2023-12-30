a9b9bd63ae8025f36a37b68a989611be8d184dc6
metal-face <hughesbryan3000@gmail.com>
2023-12-29 23:04:53 -0500

    Refactor main navigation component

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,16 +1,23 @@
+"use client";
 import * as React from "react";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export function MainNav() {
-  return (
-    <div className="h-12 flex items-center justify-end">
-      <ThemeToggle />
-      <Button variant="outline" className="m-2">
-        Login
-        <GitHubLogoIcon className="h-4 w-4 m-2" />
-      </Button>
-    </div>
-  );
+    return (
+        <div className="h-12 flex items-center justify-end">
+            <Button variant="outline">
+                <Link href="/builder">Build 🛠️</Link>
+            </Button>
+            <Button variant="outline" className="m-2">
+                Login
+                <GitHubLogoIcon className="h-4 w-4 m-2" />
+            </Button>
+            <div className="mr-2">
+                <ThemeToggle />
+            </div>
+        </div>
+    );
 }
