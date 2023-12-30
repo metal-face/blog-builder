81f104e4b9167f763fa2d371232b7630b5174eee
metal-face <hughesbryan3000@gmail.com>
2023-12-29 20:31:50 -0500

    Remove unnecessary padding in TypographyH2 component

--- components/typography/typography-h2.tsx
+++ components/typography/typography-h2.tsx
@@ -3,7 +3,7 @@ interface TypographyH2Props {
 }
 export function TypographyH2({ text }: TypographyH2Props) {
   return (
-    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+    <h2 className="scroll-m-20 border-b text-3xl font-semibold tracking-tight first:mt-0">
       {text}
     </h2>
   );
