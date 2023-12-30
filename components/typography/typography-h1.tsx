c1fa408e7c15a11830d99df0d3bdfa2aca9225db
metal-face <hughesbryan3000@gmail.com>
2023-12-29 20:31:54 -0500

    Remove unnecessary margin class from TypographyH1 component

--- components/typography/typography-h1.tsx
+++ components/typography/typography-h1.tsx
@@ -4,7 +4,7 @@ interface TypographyH1Props {
 
 export function TypographyH1({ text }: TypographyH1Props) {
   return (
-    <h1 className="m-1 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
       {text}
     </h1>
   );
