14329b2ecad52f25b2c9b07e4b4de847d0b40d78
metal-face <hughesbryan3000@gmail.com>
2023-12-29 20:31:44 -0500

    Refactor TypographyH4 component to remove default value for text prop

--- components/typography/typography-h4.tsx
+++ components/typography/typography-h4.tsx
@@ -2,9 +2,7 @@ interface TypographyH4Props {
   text: string;
 }
 
-export function TypographyH4({
-  text = new Date().toLocaleTimeString(),
-}: TypographyH4Props) {
+export function TypographyH4({ text }: TypographyH4Props) {
   return (
     <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">{text}</h4>
   );
