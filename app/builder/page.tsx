a39918e304cdcc658e3af7b749c0d1376ca769ed
metal-face <hughesbryan3000@gmail.com>
2024-01-01 15:26:57 -0500

    Add client-side rendering and sanitize HTML in markdown preview

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 import dynamic from "next/dynamic";
@@ -6,17 +7,33 @@ import { SetStateAction, useState } from "react";
 // TODO figure out wtf is going on here
 // @ts-ignore
 import * as commands from "@uiw/react-md-editor/commands";
+import { AppBar } from "@/components/app-bar";
+
+import rehypeSanitize from "rehype-sanitize";
 
 const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
 
 function BlogBuilder() {
-    const [value, setValue] = useState("**Hello world!!!**");
+    const [value, setValue] = useState(
+        `**Hello world!!!** <IFRAME SRC=\"javascript:javascript:alert(window.origin);\"></IFRAME>`
+    );
 
     return (
-        <div>
+        <div className="container">
+            <AppBar></AppBar>
+            <div className="wmde-markdown-var"> </div>
             {/* TODO figure out wtf is going on here */}
             {/* @ts-ignore */}
-            <MDEditor value={value} onChange={setValue} />
+
+            <MDEditor
+                value={value}
+                onChange={setValue}
+                height="80vh"
+                visiableDragbar={false}
+                previewOptions={{
+                    rehypePlugins: [[rehypeSanitize]],
+                }}
+            />
         </div>
     );
 }
