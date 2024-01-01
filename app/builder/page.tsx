75dd94f3f7ebfa515856d12d5dd67229578b0ddd
metal-face <hughesbryan3000@gmail.com>
2024-01-01 14:49:26 -0500

    Add markdown editor and preview components

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,5 +1,24 @@
-import { Suspense } from "react";
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+import dynamic from "next/dynamic";
+import { SetStateAction, useState } from "react";
 
-export default function Builder() {
-    return <div></div>;
+// TODO figure out wtf is going on here
+// @ts-ignore
+import * as commands from "@uiw/react-md-editor/commands";
+
+const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
+
+function BlogBuilder() {
+    const [value, setValue] = useState("**Hello world!!!**");
+
+    return (
+        <div>
+            {/* TODO figure out wtf is going on here */}
+            {/* @ts-ignore */}
+            <MDEditor value={value} onChange={setValue} />
+        </div>
+    );
 }
+
+export default BlogBuilder;
