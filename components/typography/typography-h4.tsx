4fb6edf0c339f34a746e55b0aa206f7322f9f5d5
metal-face <hughesbryan3000@gmail.com>
2024-01-01 22:08:05 -0500

    Refactor typography-h4 component

--- components/typography/typography-h4.tsx
+++ components/typography/typography-h4.tsx
@@ -1,9 +1,11 @@
 interface TypographyH4Props {
-  text: string;
+    text: string;
 }
 
 export function TypographyH4({ text }: TypographyH4Props) {
-  return (
-    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">{text}</h4>
-  );
+    return (
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+            {text}
+        </h4>
+    );
 }
