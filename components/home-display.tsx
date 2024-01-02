b0efab3632e321ea017b73b1539b05d48cc551b9
metal-face <hughesbryan3000@gmail.com>
2024-01-01 22:07:33 -0500

    Remove Clock component from HomeDisplay

--- components/home-display.tsx
+++ components/home-display.tsx
@@ -1,13 +1,11 @@
 import { TypographyH1 } from "@/components/typography/typography-h1";
-import { Clock } from "@/components/clock";
 import TypographyH3 from "./typography/typography-h3";
 
 export function HomeDisplay() {
-  return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center">
-      <TypographyH1 text={"Build Your Own Blogs"} />
-      <TypographyH3 text={"Built with ❤️ by MetalFace"} />
-      <Clock />
-    </div>
-  );
+    return (
+        <div className="h-screen w-screen flex flex-col items-center justify-center">
+            <TypographyH1 text={"Build Your Own Blogs"} />
+            <TypographyH3 text={"Built with ❤️ by MetalFace"} />
+        </div>
+    );
 }
