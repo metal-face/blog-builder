4fef9e4c06984fe4f01e1be832246e2ce7e32d7b
metal-face <hughesbryan3000@gmail.com>
2024-01-01 22:06:58 -0500

    removed mdeditor

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,43 +1,12 @@
 "use client";
 
-import "@uiw/react-md-editor/markdown-editor.css";
-import "@uiw/react-markdown-preview/markdown.css";
-import dynamic from "next/dynamic";
-import { useState } from "react";
 import rehypeSanitize from "rehype-sanitize";
 
-// TODO figure out wtf is going on here
-import { AppBar } from "@/components/app-bar";
-
-const MDEditor = dynamic(
-    () => import("@uiw/react-md-editor").then((mod) => mod.default),
-    { ssr: false }
-);
-
 function BlogBuilder() {
-    const [value, setValue] = useState(
-        `**Hello world!!!** <IFRAME SRC=\"javascript:javascript:alert(window.origin);\"></IFRAME>`
-    );
-
     return (
         <div>
-            <AppBar></AppBar>
-            <div className="container">
-                {/* TODO - figure out why I need app bar here */}
-
-                <MDEditor
-                    value={value}
-                    /* @ts-ignore */
-                    onChange={setValue}
-                    /* @ts-ignore */
-                    height="80vh"
-                    autoFocus
-                    visiableDragbar={false}
-                    previewOptions={{
-                        rehypePlugins: [[rehypeSanitize]],
-                    }}
-                />
-            </div>
+            {/* TODO - figure out why I need app bar here */}
+            <div className="h-full"></div>
         </div>
     );
 }
