e594c074479f0013c65529c1a74c163f6d4351b0
metal-face <hughesbryan3000@gmail.com>
2024-01-02 23:11:46 -0500

    Refactor tip-tap component to add new features

--- components/tip-tap.js
+++ components/tip-tap.js
@@ -1,28 +1,75 @@
-'use client'
+"use client";
 
-import { useEditor, EditorContent } from '@tiptap/react'
-import { Button } from './ui/button'
-import { FontBoldIcon } from '@radix-ui/react-icons'
-import StarterKit from '@tiptap/starter-kit'
+import { useEditor, EditorContent } from "@tiptap/react";
+import { Button } from "./ui/button";
+import {
+    FontBoldIcon,
+    FontItalicIcon,
+    StrikethroughIcon,
+    UnderlineIcon,
+} from "@radix-ui/react-icons";
+import StarterKit from "@tiptap/starter-kit";
+import Underline from "@tiptap/extension-underline";
+
+Underline.configure({
+    HTMLAttributes: {
+        class: "my-custom-class",
+    },
+});
 
 const Tiptap = () => {
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-    ],
-    content: '<p>Hello World! 🌎️</p>',
-  })
+    const editor = useEditor({
+        extensions: [StarterKit, Underline],
+        content: "<p>Hello World! 🌎️</p>",
+    });
 
-  return (
-    <div className="h-screen w-full">
-      <div className="w-full h-12 my-2 mx-2 px-2 flex justify-start items-center">
-        <Button variant="outline" onClick={() => editor.chain().focus().toggleBold().run()}>
-          <FontBoldIcon className="w-4 h-4"/>
-        </Button>
-      </div>
-      <EditorContent className="editor" editor={editor} />
-    </div>
-  )
-}
+    return (
+        <div className="h-screen w-full">
+            <div className="w-full h-12 flex justify-start items-center">
+                <div className="m-1">
+                    <Button
+                        variant="outline"
+                        onClick={() =>
+                            editor.chain().focus().toggleBold().run()
+                        }
+                    >
+                        <FontBoldIcon className="w-4 h-4" />
+                    </Button>
+                </div>
+                <div className="">
+                    <Button
+                        variant="outline"
+                        onClick={() =>
+                            editor.chain().focus().toggleItalic().run()
+                        }
+                    >
+                        <FontItalicIcon className="w-4 h-4" />
+                    </Button>
+                </div>
+                <div className="m-1">
+                    <Button
+                        variant="outline"
+                        onClick={() => editor.commands.toggleUnderline()}
+                    >
+                        <UnderlineIcon className="w-4 h-4" />
+                    </Button>
+                </div>
+                <div className="m-1">
+                    <Button
+                        variant="outline"
+                        onClick={() =>
+                            editor.chain().focus().toggleStrike().run()
+                        }
+                    >
+                        <StrikethroughIcon className="w-4 h-4" />
+                    </Button>
+                </div>
+            </div>
+            <div className="border h-full">
+                <EditorContent className="editor" editor={editor} />
+            </div>
+        </div>
+    );
+};
 
-export default Tiptap
+export default Tiptap;
