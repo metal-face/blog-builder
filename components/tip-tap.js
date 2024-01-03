cd2772b2a59d83c61f311306f4754d9ca57a9fd9
metal-face <hughesbryan3000@gmail.com>
2024-01-02 22:49:31 -0500

    bold functionality added

--- components/tip-tap.js
+++ components/tip-tap.js
@@ -1,8 +1,9 @@
 'use client'
 
 import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
 import { Button } from './ui/button'
+import { FontBoldIcon } from '@radix-ui/react-icons'
+import StarterKit from '@tiptap/starter-kit'
 
 const Tiptap = () => {
   const editor = useEditor({
@@ -13,16 +14,14 @@ const Tiptap = () => {
   })
 
   return (
-    <>
-      <div className="editor h-full w-full">
-        <div className="toolbar w-full h-14">
-          <Button>
-            
-          </Button>
-        </div>
-        <EditorContent editor={editor} />
+    <div className="h-screen w-full">
+      <div className="w-full h-12 my-2 mx-2 px-2 flex justify-start items-center">
+        <Button variant="outline" onClick={() => editor.chain().focus().toggleBold().run()}>
+          <FontBoldIcon className="w-4 h-4"/>
+        </Button>
       </div>
-    </>
+      <EditorContent className="editor" editor={editor} />
+    </div>
   )
 }
 
