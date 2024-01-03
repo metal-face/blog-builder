75e4e197480f3c2b548ddd5c4dceb6d4e8518a3d
metal-face <hughesbryan3000@gmail.com>
2024-01-02 20:08:46 -0500

    Add SlateEditor component to page.tsx

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import rehypeSanitize from "rehype-sanitize";
+import SlateEditor from "./slate-editor";
 
 function BlogBuilder() {
     return (
         <div>
             {/* TODO - figure out why I need app bar here */}
-            <div className="h-full"></div>
+            <div className="h-full">
+                <SlateEditor></SlateEditor>
+            </div>
         </div>
     );
 }
