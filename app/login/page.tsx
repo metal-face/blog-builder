de425f3000df8183b6df004745325e5c93dbcc68
metal-face <hughesbryan3000@gmail.com>
2024-01-05 17:47:39 -0500

    Login page started

---
+++ app/login/page.tsx
@@ -0,0 +1,20 @@
+import { LoginButton } from "@/components/login-button";
+import { TypographyH1 } from "@/components/typography/typography-h1";
+import { TypographyP } from "@/components/typography/typography-p";
+import { Fragment } from "react";
+
+export default async function Login() {
+    return (
+        <Fragment>
+            <div className="container flex items-center justify-center">
+                <div className="mx-auto flex w-full flex-col justify-center space-y-6 text-center">
+                    <div className="flex flex-col space-y-2 text-center">
+                        <TypographyH1 text="Welcome!" />
+                        <TypographyP text="Login to start building your own blogs." />
+                    </div>
+                    <LoginButton redirectTo="/" />
+                </div>
+            </div>
+        </Fragment>
+    );
+}
