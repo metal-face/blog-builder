85aace644ae1ede27108fa108176609075a6f482
metal-face <hughesbryan3000@gmail.com>
2024-01-05 17:48:26 -0500

    session provider added to providers

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,19 +1,22 @@
 "use client";
 import { ThemeProvider } from "next-themes";
+import { SessionProvider } from "next-auth/react";
 
 interface ProviderProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export function Providers({ children }: ProviderProps) {
-  return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
-  );
+    return (
+        <SessionProvider>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+            </ThemeProvider>
+        </SessionProvider>
+    );
 }
