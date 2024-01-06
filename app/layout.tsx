b7d1c3a364289791482f3018e793284b25170372
metal-face <hughesbryan3000@gmail.com>
2024-01-06 00:15:01 -0500

    Update layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { Providers } from "@/components/providers";
 import { AppBar } from "@/components/app-bar";
+import "./globals.css";
+import "@/app/login/style.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,12 @@ export default function RootLayout({
                 <title>Blog Builder</title>
             </head>
             <body className={inter.className}>
-                <Providers>
-                    <AppBar />
-                    <div className="h-full">{children}</div>
-                </Providers>
+                <div className="h-screen w-screen">
+                    <Providers>
+                        <AppBar />
+                        <div className="content w-full">{children}</div>
+                    </Providers>
+                </div>
             </body>
         </html>
     );
