851663628c7cb7ccec293e02b645ed9e21f9b382
metal-face <hughesbryan3000@gmail.com>
2024-01-06 00:26:05 -0500

    Update home display text

--- components/home-display.tsx
+++ components/home-display.tsx
@@ -1,11 +1,13 @@
 import { TypographyH1 } from "@/components/typography/typography-h1";
 import TypographyH3 from "./typography/typography-h3";
+import { TypographyP } from "./typography/typography-p";
 
 export function HomeDisplay() {
     return (
         <div className="h-full w-full flex flex-col items-center justify-center">
-            <TypographyH1 text={"Build Your Own Blogs"} />
-            <TypographyH3 text={"Built with ❤️ by MetalFace"} />
+            <TypographyH1 text={"Blog Builder"} />
+            <TypographyH3 text={"Craft your own blogs for free."} />
+            <TypographyP text={"Built with ❤️ by MetalFace"} />
         </div>
     );
 }
