f97540894dfbf58bf29b384bb1280e1123292e96
metal-face <hughesbryan3000@gmail.com>
2024-01-06 00:15:27 -0500

    Add style.css import to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,11 +2,12 @@ import { LoginButton } from "@/components/login-button";
 import { TypographyH1 } from "@/components/typography/typography-h1";
 import { TypographyP } from "@/components/typography/typography-p";
 import { Fragment } from "react";
+import "@/app/login/style.css";
 
 export default async function Login() {
     return (
         <Fragment>
-            <div className="container flex items-center justify-center">
+            <div className="content flex items-center justify-center">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 text-center">
                     <div className="flex flex-col space-y-2 text-center">
                         <TypographyH1 text="Welcome!" />
