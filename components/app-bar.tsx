212c9c475a41bab7d3db30948eee4ebbe44e69e0
metal-face <hughesbryan3000@gmail.com>
2024-01-07 22:44:06 -0500

    Delete unused components

--- components/app-bar.tsx
+++
@@ -1,9 +0,0 @@
-import { MainNav } from "@/components/main-nav";
-
-export function AppBar() {
-  return (
-    <header className="h-12 sticky top-0 z-50 w-full">
-      <MainNav />
-    </header>
-  );
-}
