842b08ecdf96c6a91a792b0d7d20f81b2de2f1ff
metal-face <hughesbryan3000@gmail.com>
2024-01-07 22:43:52 -0500

    Add Denied page component

---
+++ app/denied/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TypographyH1 } from "@/components/typography/typography-h1";
+
+const Denied = () => {
+    return (
+        <div className="h-full w-full flex items-center justify-center">
+            <TypographyH1 text="Permission Denied" />
+        </div>
+    );
+};
+
+export default Denied;
