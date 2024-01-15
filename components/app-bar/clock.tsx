13cb56916bc8da1e9770ff9756ed4bc1def2c703
metal-face <hughesbryan3000@gmail.com>
2024-01-14 19:01:31 -0500

    Refactor Clock component to use useMemo for options

--- components/app-bar/clock.tsx
+++ components/app-bar/clock.tsx
@@ -1,17 +1,29 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { TypographyH4 } from "@/components/typography/typography-h4";
 
 export default function Clock() {
-    const [time, setTime] = useState<string>(new Date().toLocaleTimeString());
+    const locale: Intl.LocalesArgument = "en-US";
+
+    const options: Intl.DateTimeFormatOptions = useMemo(
+        () => ({
+            hour: "2-digit",
+            minute: "2-digit",
+        }),
+        []
+    );
+
+    const [time, setTime] = useState<string>(
+        new Date().toLocaleTimeString(locale, options)
+    );
 
     useEffect(() => {
         const intervalID = setInterval(() => {
-            setTime(new Date().toLocaleTimeString());
+            setTime(new Date().toLocaleTimeString(locale, options));
         }, 1000);
 
         return () => clearInterval(intervalID);
-    }, []);
+    }, [options]);
 
     if (!time) return null;
 
