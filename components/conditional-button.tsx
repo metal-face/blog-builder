d3357f6c24540ea3fb915013dcc59d9cea9e5c32
metal-face <hughesbryan3000@gmail.com>
2024-01-14 19:02:00 -0500

    Add usePathname hook to ConditionalButton component

--- components/conditional-button.tsx
+++ components/conditional-button.tsx
@@ -1,19 +1,23 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
 
 interface ConditionalButtonProps {
     name: string;
-    visible: boolean;
+    path: string;
     classes?: string;
     children?: React.ReactNode;
 }
 
 export default function ConditionalButton({
     name,
-    visible,
+    path,
     classes,
     children,
 }: ConditionalButtonProps) {
-    return visible ? (
+    const currentPath = usePathname();
+
+    return path !== currentPath ? (
         <Button variant="outline" className={classes}>
             {name}
             {children}
