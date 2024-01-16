c0b8d9a5eb2d655e5fbdd76dffabd89f71310b36
metal-face <hughesbryan3000@gmail.com>
2024-01-15 19:08:02 -0500

    Refactor AppBar component to remove unnecessary code

--- components/app-bar/app-bar.tsx
+++ components/app-bar/app-bar.tsx
@@ -1,9 +1,5 @@
 import { MainNav } from "@/components/app-bar/main-nav";
 
 export function AppBar() {
-  return (
-    <header className="h-12 sticky top-0 z-50 w-full">
-      <MainNav />
-    </header>
-  );
+    return <MainNav />;
 }
