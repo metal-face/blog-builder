789b53702a3430f79f13fbe187affb9df750ed82
metal-face <hughesbryan3000@gmail.com>
2024-01-17 03:38:13 -0500

    second button for google started

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -10,25 +10,37 @@ type State = "error" | "idle" | "pending" | "success";
 export function LoginButton({ redirectTo }: { redirectTo: string }) {
     const [state, setState] = useState<State>("idle");
 
-    const handleSignIn = async () => {
+    async function handleSignIn(provider: string) {
         try {
             setState("pending");
-            await signIn("github", { callbackUrl: redirectTo });
+            await signIn(provider, { callbackUrl: redirectTo });
         } catch (error) {
             setState("error");
         }
-    };
+    }
 
     return (
-        <div className="mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#c63131]">
-            <Button
-                variant="outline"
-                onClick={handleSignIn}
-                disabled={state === "pending"}
-                className="fancy-border-gradient hover:bg-background relative mx-auto flex gap-4 border-none"
-            >
-                Login with GitHub <GitHubLogo />
-            </Button>
-        </div>
+        <>
+            <div className="mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#c63131]">
+                <Button
+                    variant="outline"
+                    onClick={() => handleSignIn("github")}
+                    disabled={state === "pending"}
+                    className="fancy-border-gradient hover:bg-background relative mx-auto flex gap-4 border-none"
+                >
+                    Login with GitHub <GitHubLogo />
+                </Button>
+            </div>
+            <div className="mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#c63131]">
+                <Button
+                    variant="outline"
+                    onClick={() => handleSignIn("google")}
+                    disabled={state === "pending"}
+                    className="fancy-border-gradient hover:bg-background relative mx-auto flex gap-4 border-none"
+                >
+                    Login with Google <GitHubLogo />
+                </Button>
+            </div>
+        </>
     );
 }
