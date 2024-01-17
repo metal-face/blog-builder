ff8e240de5955bc4a58d3cc94b7688be241d1877
metal-face <hughesbryan3000@gmail.com>
2024-01-17 12:52:40 -0500

    Refactor authentication code and update login page layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,7 @@ import "@/app/login/style.css";
 
 export default async function Login() {
     return (
-        <div className="content flex items-center justify-center">
+        <div className="content flex w-screen items-center justify-center">
             <div className="flex h-fit w-full flex-col justify-center space-y-3  text-center">
                 <div className="flex flex-col space-y-0 text-center">
                     <TypographyH1 text="Welcome!" />
