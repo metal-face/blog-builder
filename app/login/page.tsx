1620181743b33990a4894030aec2dc56e147cc77
metal-face <hughesbryan3000@gmail.com>
2024-01-17 04:18:37 -0500

    component name updated

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,4 @@
-import { LoginButton } from "@/components/login-button";
+import { LoginButtons } from "@/components/login-buttons";
 import { TypographyH1 } from "@/components/typography/typography-h1";
 import { TypographyP } from "@/components/typography/typography-p";
 import { Fragment } from "react";
@@ -8,16 +8,14 @@ import "@/app/login/style.css";
 
 export default async function Login() {
     return (
-        <Fragment>
-            <div className="content flex items-center justify-center">
-                <div className="mx-auto flex w-full flex-col justify-center space-y-6 text-center">
-                    <div className="flex flex-col space-y-2 text-center">
-                        <TypographyH1 text="Welcome!" />
-                        <TypographyP text="Login to start building your own blogs." />
-                    </div>
-                    <LoginButton redirectTo="/" />
+        <div className="content flex items-center justify-center">
+            <div className="mx-auto flex w-full flex-col justify-center space-y-6 text-center">
+                <div className="flex flex-col space-y-0 text-center">
+                    <TypographyH1 text="Welcome!" />
+                    <TypographyP text="Login to start building your own blogs." />
                 </div>
+                <LoginButtons redirectTo="/" />
             </div>
-        </Fragment>
+        </div>
     );
 }
