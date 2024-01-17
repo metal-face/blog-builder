bb8bb3a6311d817307feab16e5b350681ab3ad19
metal-face <hughesbryan3000@gmail.com>
2024-01-17 04:18:17 -0500

    component renamed

--- components/login-buttons.tsx
+++ components/login-buttons.tsx
@@ -4,10 +4,11 @@ import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import GitHubLogo from "./icons/github";
+import { FaGoogle } from "react-icons/fa";
 
 type State = "error" | "idle" | "pending" | "success";
 
-export function LoginButton({ redirectTo }: { redirectTo: string }) {
+export function LoginButtons({ redirectTo }: { redirectTo: string }) {
     const [state, setState] = useState<State>("idle");
 
     async function handleSignIn(provider: string) {
@@ -21,24 +22,26 @@ export function LoginButton({ redirectTo }: { redirectTo: string }) {
 
     return (
         <>
-            <div className="mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#c63131]">
+            <div className="mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#FFF]">
                 <Button
                     variant="outline"
-                    onClick={() => handleSignIn("github")}
-                    disabled={state === "pending"}
-                    className="fancy-border-gradient hover:bg-background relative mx-auto flex gap-4 border-none"
-                >
-                    Login with GitHub <GitHubLogo />
-                </Button>
-            </div>
-            <div className="mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#c63131]">
-                <Button
-                    variant="outline"
+                    size="lg"
                     onClick={() => handleSignIn("google")}
                     disabled={state === "pending"}
-                    className="fancy-border-gradient hover:bg-background relative mx-auto flex gap-4 border-none"
+                    className="google hover:bg-background relative mx-auto flex gap-4 border-none"
                 >
-                    Login with Google <GitHubLogo />
+                    Login with Google <FaGoogle />
+                </Button>
+            </div>
+            <div className="mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#FFF]">
+                <Button
+                    variant="outline"
+                    size="lg"
+                    onClick={() => handleSignIn("github")}
+                    disabled={state === "pending"}
+                    className="github hover:bg-background relative mx-auto flex gap-4 border-none"
+                >
+                    Login with GitHub <GitHubLogo />
                 </Button>
             </div>
         </>
