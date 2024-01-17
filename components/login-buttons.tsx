f7d4eb76234ca91d0317c26423b342773fda0f0c
metal-face <hughesbryan3000@gmail.com>
2024-01-17 12:11:01 -0500

    Add Discord login button

--- components/login-buttons.tsx
+++ components/login-buttons.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import GitHubLogo from "./icons/github";
 import { FaGoogle } from "react-icons/fa";
+import { FaDiscord } from "react-icons/fa";
 
 type State = "error" | "idle" | "pending" | "success";
 
@@ -22,18 +23,7 @@ export function LoginButtons({ redirectTo }: { redirectTo: string }) {
 
     return (
         <>
-            <div className="mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#FFF]">
-                <Button
-                    variant="outline"
-                    size="lg"
-                    onClick={() => handleSignIn("google")}
-                    disabled={state === "pending"}
-                    className="google hover:bg-background relative mx-auto flex gap-4 border-none"
-                >
-                    Login with Google <FaGoogle />
-                </Button>
-            </div>
-            <div className="mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#FFF]">
+            <div className="h-fit mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#b1d6fb]">
                 <Button
                     variant="outline"
                     size="lg"
@@ -42,6 +32,29 @@ export function LoginButtons({ redirectTo }: { redirectTo: string }) {
                     className="github hover:bg-background relative mx-auto flex gap-4 border-none"
                 >
                     Login with GitHub <GitHubLogo />
+                </Button>
+            </div>
+
+            <div className="h-fit mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#5865F2]">
+                <Button
+                    variant="outline"
+                    size="lg"
+                    onClick={() => handleSignIn("discord")}
+                    disabled={state === "pending"}
+                    className="discord hover:bg-background relative mx-auto flex gap-4 border-none"
+                >
+                    Login with Discord <FaDiscord />
+                </Button>
+            </div>
+            <div className="h-fit mx-auto rounded-md duration-300 hover:shadow-[0_0_2rem_-0.5rem_#fbbc05]">
+                <Button
+                    variant="outline"
+                    size="lg"
+                    onClick={() => handleSignIn("google")}
+                    disabled={state === "pending"}
+                    className="google hover:bg-background relative mx-auto flex gap-4 border-none"
+                >
+                    Login with Google <FaGoogle />
                 </Button>
             </div>
         </>
