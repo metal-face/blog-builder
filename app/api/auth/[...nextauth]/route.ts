baa884c3e642e4895af45b3964748c7a8f0819b0
metal-face <hughesbryan3000@gmail.com>
2024-01-19 11:45:52 -0500

    Add LinkedInProvider to authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,8 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import DiscordProvider from "next-auth/providers/discord";
 import TwitterProvider from "next-auth/providers/twitter";
+import LinkedInProvider from "next-auth/providers/linkedin";
+
 import type { NextAuthOptions } from "next-auth";
 
 if (!process.env.DISCORD_CLIENT_ID) {
@@ -32,6 +34,27 @@ const scopes = ["identify", "email"];
 
 export const authOptions: NextAuthOptions = {
     providers: [
+        LinkedInProvider({
+            clientId: process.env.LINKEDIN_ID as string,
+            clientSecret: process.env.LINKEDIN_SECRET as string,
+            authorization: {
+                params: {
+                    scope: "openid profile email",
+                },
+            },
+            issuer: "https://www.linkedin.com",
+            jwks_endpoint: "https://www.linkedin.com/oauth/openid/jwks",
+            profile(profile, tokens) {
+                const defaultImage =
+                    "https://cdn-icons-png.flaticon.com/512/174/174857.png";
+                return {
+                    id: profile.sub,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture ?? defaultImage,
+                };
+            },
+        }),
         TwitterProvider({
             clientId: process.env.TWITTER_ID as string,
             clientSecret: process.env.TWITTER_SECRET as string,
