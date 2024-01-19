aa8e8811d5fabd1b690a6d67b3cf5000e3bc04b6
metal-face <hughesbryan3000@gmail.com>
2024-01-18 19:59:40 -0500

    Add session prop to ProfileCard component

--- components/app-bar/profile-card.tsx
+++ components/app-bar/profile-card.tsx
@@ -3,14 +3,46 @@ import {
     HoverCardContent,
     HoverCardTrigger,
 } from "@/components/ui/hover-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
 
-interface ProfileCardProps {}
+import { Session } from "next-auth";
+import { TypographyP } from "../typography/typography-p";
 
-export default function ProfileCard() {
+interface ProfileCardProps {
+    session: Session | null;
+}
+
+export default function ProfileCard({ session }: ProfileCardProps) {
     return (
         <HoverCard>
-            <HoverCardTrigger>User Name</HoverCardTrigger>
-            <HoverCardContent>User details</HoverCardContent>
+            <HoverCardTrigger className="text-sm">
+                {session?.user?.name}
+            </HoverCardTrigger>
+            <HoverCardContent className="flex text-xs">
+                <div className="w-1/3">
+                    {session?.user?.image ? (
+                        <Avatar>
+                            <AvatarImage
+                                src={session.user.image}
+                                alt="User Profile Image"
+                            />
+                            {/* <AvatarFallback>
+                                <Image
+                                    src={session.user.image}
+                                    alt="User Profile Image"
+                                    layout="fill"
+                                />
+                            </AvatarFallback> */}
+                        </Avatar>
+                    ) : null}
+                </div>
+                <div className="w-2/3 text-xs">
+                    {session?.user?.email ? (
+                        <TypographyP text={session.user.email} />
+                    ) : null}
+                </div>
+            </HoverCardContent>
         </HoverCard>
     );
 }
