58202f54a7a01cda6f4e4a7b7c4fff36f454d35b
metal-face <hughesbryan3000@gmail.com>
2024-01-18 20:12:06 -0500

    Update profile card text class

--- components/app-bar/profile-card.tsx
+++ components/app-bar/profile-card.tsx
@@ -32,7 +32,7 @@ export default function ProfileCard({ session }: ProfileCardProps) {
                             </AvatarFallback> */}
                     </Avatar>
                 ) : (
-                    <p className="text-xs cursor-pointer mr-1">
+                    <p className="text-xs cursor-default mr-1">
                         {session?.user?.name}
                     </p>
                 )}
