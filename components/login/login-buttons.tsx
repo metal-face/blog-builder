4e70636ef968ee946c0491fbd3459f62eaac13eb
metal-face <hughesbryan3000@gmail.com>
2024-01-23 02:45:40 -0500

    Twitch, Twitter, and LinkedIn auth providers removed

--- components/login/login-buttons.tsx
+++ components/login/login-buttons.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import { FaGithub, FaLink } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
 import { FaDiscord } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaTwitch } from "react-icons/fa";
 import { Providers } from "@/components/login/login-button";
 import LoginButton from "@/components/login/login-button";
 
@@ -20,15 +17,6 @@ export function LoginButtons() {
             </LoginButton>
             <LoginButton provider={Providers.GOOGLE} colorCode="#fbbc05">
                 <FaGoogle />
-            </LoginButton>
-            <LoginButton provider={Providers.LINKEDIN} colorCode="#0077B5">
-                <FaLinkedin />
-            </LoginButton>
-            <LoginButton provider={Providers.TWITCH} colorCode="#9146FF">
-                <FaTwitch />
-            </LoginButton>
-            <LoginButton provider={Providers.TWITTER} colorCode="#1DA1F2">
-                <FaTwitter />
             </LoginButton>
         </>
     );
