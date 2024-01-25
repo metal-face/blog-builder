bff047b7ba48ea8a94917005f68babb804838fab
metal-face <hughesbryan3000@gmail.com>
2024-01-25 11:14:43 -0500

    radix icons replaced with react icons

--- components/app-bar/theme-toggle.tsx
+++ components/app-bar/theme-toggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 import { useTheme } from "next-themes";
 
@@ -20,9 +20,8 @@ export function ThemeToggle() {
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
-                    <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="sr-only">Toggle Theme</span>
+                    <FaSun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <FaMoon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
