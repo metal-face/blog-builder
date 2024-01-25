752f16d14a2968be179715cd78424fcacae2d7b6
metal-face <hughesbryan3000@gmail.com>
2024-01-25 12:47:13 -0500

    role based authentication added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -42,11 +42,54 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
             authorization: { params: { scope: scopes.join(" ") } },
             allowDangerousEmailAccountLinking: true,
+            token: "https://discord.com/api/oauth2/token",
+            userinfo: "https://discord.com/api/users/@me",
+            profile(profile) {
+                let role = "user";
+
+                if (profile.email === "mail@bryanhughes.net") {
+                    role = "admin";
+                }
+
+                if (profile.avatar === null) {
+                    const defaultAvatarNumber =
+                        parseInt(profile.discriminator) % 5;
+                    profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
+                } else {
+                    const format = profile.avatar.startsWith("a_")
+                        ? "gif"
+                        : "png";
+                    profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`;
+                }
+
+                return {
+                    id: profile.id,
+                    name: profile.username,
+                    email: profile.email,
+                    image: profile.image_url,
+                    role: role,
+                };
+            },
         }),
         GoogleProvider({
             clientId: process.env.GOOGLE_ID as string,
             clientSecret: process.env.GOOGLE_SECRET as string,
             allowDangerousEmailAccountLinking: true,
+            profile(profile) {
+                let role = "user";
+
+                if (profile.email === "hughesbryan3000@gmail.com") {
+                    role = "admin";
+                } else role = "user";
+
+                return {
+                    id: profile.sub,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture,
+                    role: role,
+                };
+            },
             authorization: {
                 params: {
                     prompt: "consent",
@@ -59,21 +102,26 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
             allowDangerousEmailAccountLinking: true,
+            profile(profile) {
+                let role = "user";
+
+                if (profile.email === "hughesbryan3000@gmail.com") {
+                    role = "admin";
+                }
+
+                return {
+                    id: profile.id,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.avatar_url,
+                    role: role,
+                };
+            },
         }),
     ],
     callbacks: {
-        async jwt({ token, user }: any) {
-            // if (user) token.role = user.role;
-            return token;
-        },
-        async session({ session, token }: any) {
-            // if (
-            //     session.user.email === "hughesbryan3000@gmail.com" ||
-            //     session.user.email === "mail@bryanhughes.net"
-            // ) {
-            //     session.user.role = "admin";
-            // }
-
+        async session({ session, user }: any) {
+            session.user.role = user.role;
             return session;
         },
     },
