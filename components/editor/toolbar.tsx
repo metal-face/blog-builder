39f900db26b6a6e03ca9c68f2d70d2611fbb2d71
metal-face <hughesbryan3000@gmail.com>
2024-01-25 05:33:21 -0500

    items centered, divs removed

--- components/editor/toolbar.tsx
+++ components/editor/toolbar.tsx
@@ -10,112 +10,92 @@ import {
 import { MdFormatUnderlined } from "react-icons/md";
 import { MdFormatListNumbered } from "react-icons/md";
 import { CiBoxList } from "react-icons/ci";
-import { useCallback } from "react";
 
 export default function Toolbar({ editor }: any) {
     return (
-        <div className="w-screen h-12 flex flex-nowrap justify-start items-center">
+        <div className="w-screen h-12 flex flex-nowrap justify-center items-center">
             {/* BOLD */}
-            <div className="m-1">
-                <Button
-                    variant="outline"
-                    size={"sm"}
-                    onClick={() => editor.chain().focus().toggleBold().run()}
-                >
-                    <FaBold />
-                </Button>
-            </div>
+            <Button
+                className="m-1"
+                variant="outline"
+                size={"sm"}
+                onClick={() => editor.chain().focus().toggleBold().run()}
+            >
+                <FaBold />
+            </Button>
             {/* ITALIC */}
-            <div>
-                <Button
-                    variant="outline"
-                    size={"sm"}
-                    onClick={() => editor.chain().focus().toggleItalic().run()}
-                >
-                    <FaItalic />
-                </Button>
-            </div>
+            <Button
+                variant="outline"
+                size={"sm"}
+                onClick={() => editor.chain().focus().toggleItalic().run()}
+            >
+                <FaItalic />
+            </Button>
             {/* UNDERLINE */}
-            <div className="m-1">
-                <Button
-                    size="sm"
-                    variant="outline"
-                    onClick={() => editor.commands.toggleUnderline()}
-                >
-                    <MdFormatUnderlined />
-                </Button>
-            </div>
+            <Button
+                className="m-1"
+                size="sm"
+                variant="outline"
+                onClick={() => editor.commands.toggleUnderline()}
+            >
+                <MdFormatUnderlined />
+            </Button>
             {/* STRIKETHROUGH */}
-            <div>
-                <Button
-                    size="sm"
-                    variant="outline"
-                    onClick={() => editor.chain().focus().toggleStrike().run()}
-                >
-                    <FaStrikethrough />
-                </Button>
-            </div>
+            <Button
+                size="sm"
+                variant="outline"
+                onClick={() => editor.chain().focus().toggleStrike().run()}
+            >
+                <FaStrikethrough />
+            </Button>
             {/* CODE */}
-            <div className="m-1">
-                <Button
-                    size="sm"
-                    variant="outline"
-                    onClick={() => editor.chain().focus().toggleCode().run()}
-                >
-                    <FaCode />
-                </Button>
-            </div>
+            <Button
+                className="m-1"
+                size="sm"
+                variant="outline"
+                onClick={() => editor.chain().focus().toggleCode().run()}
+            >
+                <FaCode />
+            </Button>
             {/* HIGHLIGHT */}
-            <div>
-                <Button
-                    size="sm"
-                    variant="outline"
-                    onClick={() =>
-                        editor.commands.toggleHighlight({
-                            color: "#ffcc00",
-                        })
-                    }
-                >
-                    <FaHighlighter />
-                </Button>
-            </div>
+            <Button
+                size="sm"
+                variant="outline"
+                onClick={() =>
+                    editor.commands.toggleHighlight({
+                        color: "#ffcc00",
+                    })
+                }
+            >
+                <FaHighlighter />
+            </Button>
             {/* BULLET LIST */}
-            <div className="m-1">
-                <Button
-                    size="sm"
-                    variant="outline"
-                    onClick={() =>
-                        editor.chain().focus().toggleBulletList().run()
-                    }
-                    className={editor.isActive("bulletList") ? "is-active" : ""}
-                >
-                    <CiBoxList />
-                </Button>
-            </div>
+            <Button
+                size="sm"
+                variant="outline"
+                onClick={() => editor.chain().focus().toggleBulletList().run()}
+                className={
+                    editor.isActive("bulletList") ? "m-1 is-active" : "m-1"
+                }
+            >
+                <CiBoxList />
+            </Button>
             {/* NUMBER LIST */}
-            <div>
-                <Button
-                    size="sm"
-                    variant="outline"
-                    onClick={() =>
-                        editor.chain().focus().toggleOrderedList().run()
-                    }
-                    className={
-                        editor.isActive("orderedList") ? "is-active" : ""
-                    }
-                >
-                    <MdFormatListNumbered />
-                </Button>
-            </div>
-            <div className="ml-1">
-                <Button
-                    size="sm"
-                    variant="outline"
-                    className={editor.isActive("link") ? "is-active" : ""}
-                >
-                    <FaLink />
-                </Button>
-            </div>
+            <Button
+                size="sm"
+                variant="outline"
+                onClick={() => editor.chain().focus().toggleOrderedList().run()}
+                className={editor.isActive("orderedList") ? "is-active" : ""}
+            >
+                <MdFormatListNumbered />
+            </Button>
+            <Button
+                size="sm"
+                variant="outline"
+                className={editor.isActive("link") ? "m-1 is-active" : "m-1"}
+            >
+                <FaLink />
+            </Button>
         </div>
     );
 }
