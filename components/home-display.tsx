394d7b90857e5be62731dbeb476f90282a42a317
metal-face <hughesbryan3000@gmail.com>
2024-01-27 04:15:26 -0500

    custom class provided to calculate responsive height

--- components/home-display.tsx
+++ components/home-display.tsx
@@ -1,10 +1,10 @@
 import { TypographyH1 } from "@/components/typography/typography-h1";
-import TypographyH3 from "./typography/typography-h3";
 import { TypographyP } from "./typography/typography-p";
+import TypographyH3 from "./typography/typography-h3";
 
 export function HomeDisplay() {
     return (
-        <div className="h-full w-full flex flex-col items-center justify-center">
+        <div className="custom-container w-full flex flex-col items-center justify-center">
             <TypographyH1 text={"Blog Builder"} />
             <TypographyH3 text={"Craft your own blogs for free."} />
             <TypographyP text={"Built with ❤️ by MetalFace"} />
