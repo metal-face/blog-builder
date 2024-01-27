22aea8eb6c6d33719759687a6a7b6dee82743a3e
metal-face <hughesbryan3000@gmail.com>
2024-01-27 03:53:20 -0500

    html and css fixed up

--- components/app-bar/main-nav.tsx
+++ components/app-bar/main-nav.tsx
@@ -14,52 +14,46 @@ export async function MainNav() {
     const session = await getServerSession(authOptions);
 
     return (
-        <div className="sticky top-0 z-40 inset-x-0 backdrop-blur transition-colors duration-500 lg:border-b lg:border-slate-900/10 dark:border-slate-50[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent w-full no-flex border-b ">
-            <div className="py-4  border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 px-4">
-                <div className="relative flex justify-between items-center">
-                    <div className="h-full flex justify-start align-middle">
-                        <Clock />
-                    </div>
-                    <div className="h-full flex items-center">
-                        {session ? (
-                            <Link href="/builder">
-                                <ConditionalButton
-                                    classes="ml-1"
-                                    name="Blog "
-                                    path="/builder"
-                                >
-                                    <IoIosConstruct className="ml-2" />
-                                </ConditionalButton>
-                            </Link>
-                        ) : null}
-
-                        <Link href="/">
+        <div className="h-20 sticky top-0 z-40 inset-x-0 backdrop-blur transition-colors duration-500 lg:border-b lg:border-slate-900/10 dark:border-slate-50[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent w-full  border-b ">
+            <div className="py-4 h-full flex justify-between items-center border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 px-4">
+                <div className="h-full flex justify-start align-middle">
+                    <Clock />
+                </div>
+                <div className="h-full flex items-center">
+                    {session ? (
+                        <Link href="/builder">
                             <ConditionalButton
                                 classes="ml-1"
-                                name="Home"
-                                path="/"
+                                name="Blog "
+                                path="/builder"
                             >
-                                <FaHome className="ml-2" />
+                                <IoIosConstruct className="ml-2" />
                             </ConditionalButton>
                         </Link>
+                    ) : null}
 
-                        {!session ? (
-                            <Link href="/login">
-                                <ConditionalButton
-                                    classes="ml-1"
-                                    name="Login"
-                                    path="/login"
-                                >
-                                    <EnterIcon className="ml-2" />
-                                </ConditionalButton>
-                            </Link>
-                        ) : null}
+                    <Link href="/">
+                        <ConditionalButton classes="ml-1" name="Home" path="/">
+                            <FaHome className="ml-2" />
+                        </ConditionalButton>
+                    </Link>
 
-                        <div className="flex align-middle mx-1">
-                            <ThemeToggle />
-                        </div>
-                        {session ? <ProfileCard session={session} /> : null}
+                    {!session ? (
+                        <Link href="/login">
+                            <ConditionalButton
+                                classes="ml-1"
+                                name="Login"
+                                path="/login"
+                            >
+                                <EnterIcon className="ml-2" />
+                            </ConditionalButton>
+                        </Link>
+                    ) : null}
+
+                    <div className="flex align-middle mx-1">
+                        <ThemeToggle />
                     </div>
+                    {session ? <ProfileCard session={session} /> : null}
                 </div>
             </div>
         </div>
