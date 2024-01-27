342a384dbd03a345bfcc78f16dc6bea23598013e
metal-face <hughesbryan3000@gmail.com>
2024-01-27 04:58:16 -0500

    Add image support and update toolbar icons

--- components/editor/tip-tap.tsx
+++ components/editor/tip-tap.tsx
@@ -18,7 +18,21 @@ import Link from "@tiptap/extension-link";
 import TextStyle from "@tiptap/extension-text-style";
 import Color from "@tiptap/extension-color";
 import Toolbar from "@/components/editor/toolbar";
-import Heading from "@tiptap/extension-heading";
+import Image from "@tiptap/extension-image";
+import { useCallback } from "react";
+import { Button } from "@/components/ui/button";
+import {
+    FaCode,
+    FaLink,
+    FaBold,
+    FaItalic,
+    FaStrikethrough,
+    FaHighlighter,
+} from "react-icons/fa";
+import { MdFormatUnderlined } from "react-icons/md";
+import { MdFormatListNumbered } from "react-icons/md";
+import { CiBoxList } from "react-icons/ci";
+import { AiFillPicture } from "react-icons/ai";
 
 export default function Tiptap() {
     const form = useForm({
@@ -39,6 +53,9 @@ export default function Tiptap() {
                     keepMarks: true,
                     keepAttributes: false,
                 },
+                heading: {
+                    levels: [1, 2, 3],
+                },
             }),
             Color,
             TextStyle,
@@ -48,9 +65,7 @@ export default function Tiptap() {
                 openOnClick: false,
                 autolink: true,
             }),
-            Heading.configure({
-                levels: [1, 2, 3],
-            }),
+            Image,
         ],
         editorProps: {
             attributes: {
@@ -60,13 +75,115 @@ export default function Tiptap() {
         content: "Hello World! 🌎️",
     });
 
+    const addImage = useCallback(() => {
+        const url = window.prompt("URL");
+
+        if (url) {
+            editor?.chain().focus().setImage({ src: url }).run();
+        }
+    }, [editor]);
+
     if (!editor) {
         return null;
     }
 
     return (
         <div className="custom-container flex justify-center flex-col">
-            <Toolbar editor={editor} />
+            <div className="w-screen h-12 flex flex-nowrap justify-center items-center">
+                {/* BOLD */}
+                <Button
+                    className="m-1"
+                    variant="outline"
+                    size={"sm"}
+                    onClick={() => editor.chain().focus().toggleBold().run()}
+                >
+                    <FaBold />
+                </Button>
+                {/* ITALIC */}
+                <Button
+                    variant="outline"
+                    size={"sm"}
+                    onClick={() => editor.chain().focus().toggleItalic().run()}
+                >
+                    <FaItalic />
+                </Button>
+                {/* UNDERLINE */}
+                <Button
+                    className="m-1"
+                    size="sm"
+                    variant="outline"
+                    onClick={() => editor.commands.toggleUnderline()}
+                >
+                    <MdFormatUnderlined />
+                </Button>
+                {/* STRIKETHROUGH */}
+                <Button
+                    size="sm"
+                    variant="outline"
+                    onClick={() => editor.chain().focus().toggleStrike().run()}
+                >
+                    <FaStrikethrough />
+                </Button>
+                {/* CODE */}
+                <Button
+                    className="m-1"
+                    size="sm"
+                    variant="outline"
+                    onClick={() => editor.chain().focus().toggleCode().run()}
+                >
+                    <FaCode />
+                </Button>
+                {/* HIGHLIGHT */}
+                <Button
+                    size="sm"
+                    variant="outline"
+                    onClick={() =>
+                        editor.commands.toggleHighlight({
+                            color: "#ffcc00",
+                        })
+                    }
+                >
+                    <FaHighlighter />
+                </Button>
+                {/* BULLET LIST */}
+                <Button
+                    size="sm"
+                    variant="outline"
+                    onClick={() =>
+                        editor.chain().focus().toggleBulletList().run()
+                    }
+                    className={
+                        editor.isActive("bulletList") ? "m-1 is-active" : "m-1"
+                    }
+                >
+                    <CiBoxList />
+                </Button>
+                {/* NUMBER LIST */}
+                <Button
+                    size="sm"
+                    variant="outline"
+                    onClick={() =>
+                        editor.chain().focus().toggleOrderedList().run()
+                    }
+                    className={
+                        editor.isActive("orderedList") ? "is-active" : ""
+                    }
+                >
+                    <MdFormatListNumbered />
+                </Button>
+                <Button
+                    size="sm"
+                    variant="outline"
+                    className={
+                        editor.isActive("link") ? "m-1 is-active" : "m-1"
+                    }
+                >
+                    <FaLink />
+                </Button>
+                <Button size="sm" variant="outline" onClick={addImage}>
+                    <AiFillPicture />
+                </Button>
+            </div>
             <EditorContent editor={editor} />
         </div>
     );
