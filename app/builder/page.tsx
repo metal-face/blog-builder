d0697f2994c4896609d7b8037af2d53ed7d1127c
metal-face <hughesbryan3000@gmail.com>
2024-01-27 04:41:33 -0500

    router guard added to builder page

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,7 +1,20 @@
+"use client";
 import Tiptap from "@/components/editor/tip-tap";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-function BlogBuilder() {
-    return <Tiptap />;
+export default function BlogBuilder() {
+    const { data: session, status } = useSession();
+    const router = useRouter();
+    if (status === "unauthenticated") {
+        router.push("/login");
+    }
+
+    if (status === "loading") {
+        return (
+            <div className="custom-container flex justify-center items-center">
+                Loading...
+            </div>
+        );
+    } else return <Tiptap />;
 }
-
-export default BlogBuilder;
