61ab6fdea75fda79ff2659ddd16de2a8a261a6be
metal-face <hughesbryan3000@gmail.com>
2024-01-27 03:21:45 -0500

    Refactor Home component to use shorthand syntax for returning JSX

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,5 @@ import { HomeDisplay } from "@/components/home-display";
 import "@/app/login/style.css";
 
 export default function Home() {
-    return (
-        <main className="content">
-            <HomeDisplay />
-        </main>
-    );
+    return <HomeDisplay />;
 }
