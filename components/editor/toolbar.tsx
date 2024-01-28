6f424f517e9ebbd9af81923763cd0d0093bafc68
metal-face <hughesbryan3000@gmail.com>
2024-01-28 12:37:24 -0500

    Tooltips added to all buttons

--- components/editor/toolbar.tsx
+++ components/editor/toolbar.tsx
@@ -58,10 +58,6 @@ export default function Toolbar({ editor }: any) {
 
     return (
         <div className="w-screen h-12 flex flex-nowrap justify-center items-center">
-            <Tooltip delayDuration={100}>
-                <TooltipTrigger asChild></TooltipTrigger>
-                <TooltipContent></TooltipContent>
-            </Tooltip>
             {/* HEADING 1 */}
             <Tooltip delayDuration={100}>
                 <TooltipTrigger asChild>
@@ -166,82 +162,145 @@ export default function Toolbar({ editor }: any) {
                 <TooltipContent>Italic</TooltipContent>
             </Tooltip>
             {/* UNDERLINE */}
-            <Button
-                className="m-1"
-                size="sm"
-                variant="outline"
-                onClick={() => editor.commands.toggleUnderline()}
-            >
-                <MdFormatUnderlined />
-            </Button>
+            <Tooltip delayDuration={100}>
+                <TooltipTrigger asChild>
+                    <Button
+                        className="m-1"
+                        size="sm"
+                        variant="outline"
+                        onClick={() => editor.commands.toggleUnderline()}
+                    >
+                        <MdFormatUnderlined />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>Underline</TooltipContent>
+            </Tooltip>
             {/* STRIKETHROUGH */}
-            <Button
-                size="sm"
-                variant="outline"
-                onClick={() => editor.chain().focus().toggleStrike().run()}
-            >
-                <FaStrikethrough />
-            </Button>
+            <Tooltip delayDuration={100}>
+                <TooltipTrigger asChild>
+                    <Button
+                        size="sm"
+                        variant="outline"
+                        onClick={() =>
+                            editor.chain().focus().toggleStrike().run()
+                        }
+                    >
+                        <FaStrikethrough />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>Strikethrough</TooltipContent>
+            </Tooltip>
             {/* CODE */}
-            <Button
-                className="ml-1"
-                size="sm"
-                variant="outline"
-                onClick={() => editor.chain().focus().toggleCode().run()}
-            >
-                <FaCode />
-            </Button>
+            <Tooltip delayDuration={100}>
+                <TooltipTrigger asChild>
+                    <Button
+                        className="ml-1"
+                        size="sm"
+                        variant="outline"
+                        onClick={() =>
+                            editor.chain().focus().toggleCode().run()
+                        }
+                    >
+                        <FaCode />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>Code Block</TooltipContent>
+            </Tooltip>
             {/* BULLET LIST */}
-            <Button
-                size="sm"
-                variant="outline"
-                onClick={() => editor.chain().focus().toggleBulletList().run()}
-                className={
-                    editor.isActive("bulletList") ? "m-1 is-active" : "m-1"
-                }
-            >
-                <CiBoxList />
-            </Button>
+            <Tooltip delayDuration={100}>
+                <TooltipTrigger asChild>
+                    <Button
+                        size="sm"
+                        variant="outline"
+                        onClick={() =>
+                            editor.chain().focus().toggleBulletList().run()
+                        }
+                        className={
+                            editor.isActive("bulletList")
+                                ? "m-1 is-active"
+                                : "m-1"
+                        }
+                    >
+                        <CiBoxList />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>Bullet List</TooltipContent>
+            </Tooltip>
             {/* NUMBER LIST */}
-            <Button
-                size="sm"
-                variant="outline"
-                onClick={() => editor.chain().focus().toggleOrderedList().run()}
-                className={editor.isActive("orderedList") ? "is-active" : ""}
-            >
-                <MdFormatListNumbered />
-            </Button>
+            <Tooltip delayDuration={100}>
+                <TooltipTrigger asChild>
+                    <Button
+                        size="sm"
+                        variant="outline"
+                        onClick={() =>
+                            editor.chain().focus().toggleOrderedList().run()
+                        }
+                        className={
+                            editor.isActive("orderedList") ? "is-active" : ""
+                        }
+                    >
+                        <MdFormatListNumbered />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>Number List</TooltipContent>
+            </Tooltip>
             {/* SUBSCRIPT */}
-            <Button
-                size="sm"
-                className="ml-1"
-                variant="outline"
-                onClick={() => editor.chain().focus().toggleSubscript().run()}
-            >
-                <FaSubscript />
-            </Button>
+            <Tooltip delayDuration={100}>
+                <TooltipTrigger asChild>
+                    <Button
+                        size="sm"
+                        className="ml-1"
+                        variant="outline"
+                        onClick={() =>
+                            editor.chain().focus().toggleSubscript().run()
+                        }
+                    >
+                        <FaSubscript />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>Subscript</TooltipContent>
+            </Tooltip>
             {/* SUPERSCRIPT */}
-            <Button
-                size="sm"
-                className="ml-1"
-                variant="outline"
-                onClick={() => editor.chain().focus().toggleSuperscript().run()}
-            >
-                <FaSuperscript />
-            </Button>
+            <Tooltip delayDuration={100}>
+                <TooltipTrigger asChild>
+                    <Button
+                        size="sm"
+                        className="ml-1"
+                        variant="outline"
+                        onClick={() =>
+                            editor.chain().focus().toggleSuperscript().run()
+                        }
+                    >
+                        <FaSuperscript />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>Superscript</TooltipContent>
+            </Tooltip>
             {/* LINK */}
-            <Button
-                onClick={setLink}
-                size="sm"
-                variant="outline"
-                className={editor.isActive("link") ? "m-1 is-active" : "m-1"}
-            >
-                <FaLink />
-            </Button>
+            <Tooltip delayDuration={100}>
+                <TooltipTrigger asChild>
+                    <Button
+                        onClick={setLink}
+                        size="sm"
+                        variant="outline"
+                        className={
+                            editor.isActive("link") ? "m-1 is-active" : "m-1"
+                        }
+                    >
+                        <FaLink />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>Insert Link</TooltipContent>
+            </Tooltip>
             {/* IMAGE */}
-            <Button size="sm" variant="outline" onClick={addImage}>
-                <AiFillPicture />
-            </Button>
+            <Tooltip delayDuration={100}>
+                <TooltipTrigger asChild>
+                    <Button size="sm" variant="outline" onClick={addImage}>
+                        <AiFillPicture />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>Insert Image (from URL)</TooltipContent>
+            </Tooltip>
         </div>
     );
 }
