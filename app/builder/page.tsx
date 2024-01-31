f5ea3b1929c803c53398ad19ad0a7575eca4eeab
metal-face <hughesbryan3000@gmail.com>
2024-01-31 10:13:26 -0500

    zod validation started

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,15 +1,76 @@
+"use client";
 import Tiptap from "@/components/editor/tip-tap";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 
-import { redirect } from "next/navigation";
+export default function BlogBuilder() {
+    const FormSchema = z.object({
+        blogTitle: z
+            .string()
+            .min(4, { message: "Title must be at least 4 characters" }),
+        blogPost: z
+            .string()
+            .min(20, { message: "Blog post must be at least 20 characters" })
+            .trim(),
+    });
 
-export default async function BlogBuilder() {
-    const session = await getServerSession(authOptions);
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        mode: "onChange",
+        defaultValues: {
+            blogTitle: "Add a title!",
+            blogPost: "Hello World! 🌎️",
+        },
+    });
 
-    if (!session) {
-        return redirect("/login");
-    }
+    function onSubmit(data: z.infer<typeof FormSchema>) {}
 
-    return <Tiptap />;
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FormField
+                    control={form.control}
+                    name="blogTitle"
+                    render={({ field }) => {
+                        <FormItem>
+                            <FormLabel>Blog Title</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Enter a blog title" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>;
+                    }}
+                />
+                <FormField
+                    control={form.control}
+                    name="blogPost"
+                    render={({ field }) => {
+                        <FormItem>
+                            <FormControl>
+                                <Tiptap
+                                    blogPost={field.name}
+                                    onChange={field.onChange}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>;
+                    }}
+                />
+                <Button type="submit" variant="outline">
+                    Submit
+                </Button>
+            </form>
+        </Form>
+    );
 }
