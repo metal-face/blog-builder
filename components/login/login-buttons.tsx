c887d7ed70425bf4b0e974a4d789d3fb15804deb
metal-face <hughesbryan3000@gmail.com>
2024-01-31 08:53:12 -0500

    syntax made consistent

--- components/login/login-buttons.tsx
+++ components/login/login-buttons.tsx
@@ -12,7 +12,7 @@ export function LoginButtons() {
             <LoginButton provider={Providers.GITHUB} colorCode="#b1d6fb">
                 <FaGithub />
             </LoginButton>
-            <LoginButton provider={Providers.DISCORD} colorCode={"#1DA1F2"}>
+            <LoginButton provider={Providers.DISCORD} colorCode="#1DA1F2">
                 <FaDiscord />
             </LoginButton>
             <LoginButton provider={Providers.GOOGLE} colorCode="#fbbc05">
