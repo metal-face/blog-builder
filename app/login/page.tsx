d50a748d28921bb062ba3a92503ad55b773784c6
metal-face <hughesbryan3000@gmail.com>
2024-01-31 08:53:01 -0500

    html structure fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,16 +3,14 @@ import { TypographyH1 } from "@/components/typography/typography-h1";
 import { TypographyP } from "@/components/typography/typography-p";
 import "@/app/login/style.css";
 
-export default async function Login() {
+export default function Login() {
     return (
-        <div className="content flex w-screen items-center justify-center">
-            <div className="flex h-fit w-full flex-col justify-center space-y-3  text-center">
-                <div className="flex flex-col space-y-0 text-center">
-                    <TypographyH1 text="Welcome!" />
-                    <TypographyP text="Login to start building your own blogs." />
-                </div>
-                <LoginButtons />
+        <div className="flex custom-container w-full flex-col justify-center space-y-3  text-center">
+            <div className="flex flex-col space-y-0 text-center">
+                <TypographyH1 text="Welcome!" />
+                <TypographyP text="Login to start building your own blogs." />
             </div>
+            <LoginButtons />
         </div>
     );
 }
