d64febcd13f36ff03f64a0c854de0d56e02c9170
metal-face <hughesbryan3000@gmail.com>
2024-02-08 00:17:17 -0500

    nested styles added

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    plugins: {
+        "postcss-import": {},
+        "postcss-nested": {},
+        "tailwindcss/nesting": "postcss-nesting",
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
