8dd835207fea1480fa2bfaf2e33461368b237019
metal-face <hughesbryan3000@gmail.com>
2024-02-07 23:51:20 -0500

    react icons replaced with lucid icons

--- components/app-bar/theme-toggle.tsx
+++ components/app-bar/theme-toggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { FaSun, FaMoon } from "react-icons/fa";
+import { Sun, Moon } from "lucide-react";
 
 import { useTheme } from "next-themes";
 
@@ -20,8 +20,8 @@ export function ThemeToggle() {
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
-                    <FaSun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <FaMoon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
