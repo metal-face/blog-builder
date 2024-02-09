b019b323a219e52bfc260f8410458fd13349ea61
metal-face <hughesbryan3000@gmail.com>
2024-02-08 20:06:03 -0500

    button order changed, task list button added

--- components/editor/toolbar.tsx
+++ components/editor/toolbar.tsx
@@ -12,11 +12,12 @@ import {
     Superscript,
     Link,
     Image as ImageIcon,
+    ListTodo,
 } from "lucide-react";
 
 import { useCallback } from "react";
 import { Toggle } from "@/components/ui/toggle";
-import { Editor, BubbleMenu } from "@tiptap/react";
+import { Editor } from "@tiptap/react";
 
 type Props = {
     editor: Editor | null;
@@ -61,188 +62,186 @@ export default function Toolbar({ editor }: Props) {
     }
 
     return (
-        <BubbleMenu editor={editor} tippyOptions={{ duration: 50 }}>
-            <div className="w-fit h-auto dark:bg-zinc-700 bg-zinc-200 rounded flex flex-nowrap justify-start items-center">
-                {/* HEADING 1 */}
-                <Toggle
-                    size="sm"
-                    aria-label="Toggle Heading 1"
-                    className={
-                        editor.isActive("heading", { level: 1 })
-                            ? "is-active rounded-tl rounded-bl rounded-tr-none rounded-br-none"
-                            : "rounded-tr-none rounded-br-none rounded-bl rounded-tl"
-                    }
-                    pressed={editor.isActive("heading", { level: 1 })}
-                    onPressedChange={() => {
-                        editor
-                            .chain()
-                            .focus()
-                            .toggleHeading({ level: 1 })
-                            .run();
-                    }}
-                >
-                    <Heading1 />
-                </Toggle>
-                {/* HEADING 2 */}
-                <Toggle
-                    size={"sm"}
-                    aria-label="Toggle Heading 2"
-                    pressed={editor.isActive("heading", {
-                        level: 2,
-                    })}
-                    className={
-                        editor.isActive("heading", { level: 2 })
-                            ? "is-active rounded-none"
-                            : "rounded-none"
-                    }
-                    onPressedChange={() => {
-                        editor
-                            .chain()
-                            .focus()
-                            .toggleHeading({ level: 2 })
-                            .run();
-                    }}
-                >
-                    <Heading2 />
-                </Toggle>
-                {/* HEADING 3 */}
-                <Toggle
-                    size={"sm"}
-                    aria-label="Toggle Heading 3"
-                    pressed={editor.isActive("heading", {
-                        level: 3,
-                    })}
-                    className={
-                        editor.isActive("heading", { level: 3 })
-                            ? "is-active rounded-none"
-                            : "rounded-none"
-                    }
-                    onPressedChange={() => {
-                        editor
-                            .chain()
-                            .focus()
-                            .toggleHeading({ level: 3 })
-                            .run();
-                    }}
-                >
-                    <Heading3 />
-                </Toggle>
-                {/* BOLD */}
-                <Toggle
-                    size={"sm"}
-                    className="rounded-none"
-                    aria-label="Toggle Bold"
-                    pressed={editor.isActive("bold")}
-                    onPressedChange={() => {
-                        editor.chain().focus().toggleBold().run();
-                    }}
-                >
-                    <Bold />
-                </Toggle>
-                {/* ITALIC */}
-                <Toggle
-                    size={"sm"}
-                    className="rounded-none"
-                    aria-label="Toggle Italic"
-                    pressed={editor.isActive("italic")}
-                    onPressedChange={() => {
-                        editor.chain().focus().toggleItalic().run();
-                    }}
-                >
-                    <Italic />
-                </Toggle>
-                {/* UNDERLINE */}
-                <Toggle
-                    size={"sm"}
-                    className="rounded-none"
-                    aria-label="Toggle Underline"
-                    pressed={editor.isActive("underline")}
-                    onPressedChange={() => {
-                        editor.chain().focus().toggleUnderline().run();
-                    }}
-                >
-                    <Underline />
-                </Toggle>
-                {/* STRIKETHROUGH */}
-                <Toggle
-                    size={"sm"}
-                    className="rounded-none"
-                    aria-label="Toggle Strikethrough"
-                    pressed={editor.isActive("strike")}
-                    onPressedChange={() => {
-                        editor.chain().focus().toggleStrike().run();
-                    }}
-                >
-                    <Strikethrough />
-                </Toggle>
-                {/* BULLET LIST */}
-                <Toggle
-                    size={"sm"}
-                    className="rounded-none"
-                    aria-label="Toggle Bullet List"
-                    pressed={editor.isActive("bulletList")}
-                    onPressedChange={() => {
-                        editor.chain().focus().toggleBulletList().run();
-                    }}
-                >
-                    <List />
-                </Toggle>
-                {/* NUMBER LIST */}
-                <Toggle
-                    size={"sm"}
-                    className="rounded-none"
-                    aria-label="Toggle Number List"
-                    pressed={editor.isActive("orderedList")}
-                    onPressedChange={() => {
-                        editor.chain().focus().toggleOrderedList().run();
-                    }}
-                >
-                    <ListOrdered />
-                </Toggle>
-                {/* SUBSCRIPT */}
-                <Toggle
-                    size={"sm"}
-                    className="rounded-none"
-                    aria-label="Toggle Subscript"
-                    pressed={editor.isActive("subscript")}
-                    onPressedChange={() => {
-                        editor.chain().focus().toggleSubscript().run();
-                    }}
-                >
-                    <Subscript />
-                </Toggle>
-                {/* SUPERSCRIPT */}
-                <Toggle
-                    size={"sm"}
-                    className="rounded-none"
-                    aria-label="Toggle Superscript"
-                    pressed={editor.isActive("superscript")}
-                    onPressedChange={() => {
-                        editor.chain().focus().toggleSuperscript().run();
-                    }}
-                >
-                    <Superscript />
-                </Toggle>
-                {/* LINK */}
-                <Toggle
-                    size={"sm"}
-                    className="rounded-none"
-                    aria-label="Toggle Link"
-                    pressed={editor.isActive("link")}
-                    onPressedChange={setLink}
-                >
-                    <Link />
-                </Toggle>
-                {/* IMAGE */}
-                <Toggle
-                    size={"sm"}
-                    className="rounded-bl-none rounded-tl-none rounded-tr rounded-br"
-                    aria-label="Toggle Image"
-                    onPressedChange={addImage}
-                >
-                    <ImageIcon />
-                </Toggle>
-            </div>
-        </BubbleMenu>
+        <div className="w-full rounded-bl-none rounded-br-none h-auto dark:bg-transparent bg-zinc-200 rounded flex flex-nowrap justify-start items-center">
+            {/* HEADING 1 */}
+            <Toggle
+                size="sm"
+                aria-label="Toggle Heading 1"
+                className={
+                    editor.isActive("heading", { level: 1 })
+                        ? "is-active rounded-tl rounded-bl rounded-tr-none rounded-br-none"
+                        : "rounded-tr-none rounded-br-none rounded-bl rounded-tl"
+                }
+                pressed={editor.isActive("heading", { level: 1 })}
+                onPressedChange={() => {
+                    editor.chain().focus().toggleHeading({ level: 1 }).run();
+                }}
+            >
+                <Heading1 />
+            </Toggle>
+            {/* HEADING 2 */}
+            <Toggle
+                size={"sm"}
+                aria-label="Toggle Heading 2"
+                pressed={editor.isActive("heading", {
+                    level: 2,
+                })}
+                className={
+                    editor.isActive("heading", { level: 2 })
+                        ? "is-active rounded-none"
+                        : "rounded-none"
+                }
+                onPressedChange={() => {
+                    editor.chain().focus().toggleHeading({ level: 2 }).run();
+                }}
+            >
+                <Heading2 />
+            </Toggle>
+            {/* HEADING 3 */}
+            <Toggle
+                size={"sm"}
+                aria-label="Toggle Heading 3"
+                pressed={editor.isActive("heading", {
+                    level: 3,
+                })}
+                className={
+                    editor.isActive("heading", { level: 3 })
+                        ? "is-active rounded-none"
+                        : "rounded-none"
+                }
+                onPressedChange={() => {
+                    editor.chain().focus().toggleHeading({ level: 3 }).run();
+                }}
+            >
+                <Heading3 />
+            </Toggle>
+            {/* BOLD */}
+            <Toggle
+                size={"sm"}
+                className="rounded-none"
+                aria-label="Toggle Bold"
+                pressed={editor.isActive("bold")}
+                onPressedChange={() => {
+                    editor.chain().focus().toggleBold().run();
+                }}
+            >
+                <Bold />
+            </Toggle>
+            {/* ITALIC */}
+            <Toggle
+                size={"sm"}
+                className="rounded-none"
+                aria-label="Toggle Italic"
+                pressed={editor.isActive("italic")}
+                onPressedChange={() => {
+                    editor.chain().focus().toggleItalic().run();
+                }}
+            >
+                <Italic />
+            </Toggle>
+            {/* UNDERLINE */}
+            <Toggle
+                size={"sm"}
+                className="rounded-none"
+                aria-label="Toggle Underline"
+                pressed={editor.isActive("underline")}
+                onPressedChange={() => {
+                    editor.chain().focus().toggleUnderline().run();
+                }}
+            >
+                <Underline />
+            </Toggle>
+            {/* BULLET LIST */}
+            <Toggle
+                size={"sm"}
+                className="rounded-none"
+                aria-label="Toggle Bullet List"
+                pressed={editor.isActive("bulletList")}
+                onPressedChange={() => {
+                    editor.chain().focus().toggleBulletList().run();
+                }}
+            >
+                <List />
+            </Toggle>
+            {/* NUMBER LIST */}
+            <Toggle
+                size={"sm"}
+                className="rounded-none"
+                aria-label="Toggle Number List"
+                pressed={editor.isActive("orderedList")}
+                onPressedChange={() => {
+                    editor.chain().focus().toggleOrderedList().run();
+                }}
+            >
+                <ListOrdered />
+            </Toggle>
+            {/* TASK LIST */}
+            <Toggle
+                size="sm"
+                className="rounded-none"
+                aria-label="Toggle Task List"
+                pressed={editor.isActive("taskList")}
+                onPressedChange={() => {
+                    editor.chain().focus().toggleTaskList().run();
+                }}
+            >
+                <ListTodo />
+            </Toggle>
+            {/* STRIKETHROUGH */}
+            <Toggle
+                size={"sm"}
+                className="rounded-none"
+                aria-label="Toggle Strikethrough"
+                pressed={editor.isActive("strike")}
+                onPressedChange={() => {
+                    editor.chain().focus().toggleStrike().run();
+                }}
+            >
+                <Strikethrough />
+            </Toggle>
+            {/* SUBSCRIPT */}
+            <Toggle
+                size={"sm"}
+                className="rounded-none"
+                aria-label="Toggle Subscript"
+                pressed={editor.isActive("subscript")}
+                onPressedChange={() => {
+                    editor.chain().focus().toggleSubscript().run();
+                }}
+            >
+                <Subscript />
+            </Toggle>
+            {/* SUPERSCRIPT */}
+            <Toggle
+                size={"sm"}
+                className="rounded-none"
+                aria-label="Toggle Superscript"
+                pressed={editor.isActive("superscript")}
+                onPressedChange={() => {
+                    editor.chain().focus().toggleSuperscript().run();
+                }}
+            >
+                <Superscript />
+            </Toggle>
+            {/* LINK */}
+            <Toggle
+                size={"sm"}
+                className="rounded-none"
+                aria-label="Toggle Link"
+                pressed={editor.isActive("link")}
+                onPressedChange={setLink}
+            >
+                <Link />
+            </Toggle>
+            {/* IMAGE */}
+            <Toggle
+                size={"sm"}
+                className="rounded-bl-none rounded-tl-none rounded-tr rounded-br"
+                aria-label="Toggle Image"
+                onPressedChange={addImage}
+            >
+                <ImageIcon />
+            </Toggle>
+        </div>
     );
 }
