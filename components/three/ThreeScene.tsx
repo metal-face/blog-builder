659ce829251d93bc428525ceb0f21d6af4fa5dbb
metal-face <hughesbryan3000@gmail.com>
2024-02-20 00:23:15 -0500

    basic cube component created

--- components/three/ThreeScene.tsx
+++ components/three/ThreeScene.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useRef, useEffect, FC } from "react";
 import * as Three from "three";
 
@@ -7,6 +9,11 @@ const ThreeScene: FC = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
       const scene = new Three.Scene();
+      const geometry = new Three.BoxGeometry();
+      const material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new Three.Mesh(geometry, material);
+      scene.add(cube);
+
       const camera = new Three.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
@@ -18,9 +25,34 @@ const ThreeScene: FC = () => {
       renderer.setSize(window.innerWidth, window.innerHeight);
       containerRef.current?.appendChild(renderer.domElement);
       camera.position.z = 5;
+
+      const renderScene = () => {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+        requestAnimationFrame(renderScene);
+      };
+
+      const handleResize = () => {
+        const width = window.innerHeight;
+        const height = window.innerWidth;
+
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(width, height);
+      };
+
+      renderScene();
+
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
     }
   }, []);
-  return <div ref={containerRef}></div>;
+  return <div ref={containerRef} />;
 };
 
 export default ThreeScene;
