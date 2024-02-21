1fcc48649cf1ecbcc230d39f0ad468a4ca9963bc
metal-face <hughesbryan3000@gmail.com>
2024-02-21 12:52:00 -0500

    3js pen added

--- components/home-display.tsx
+++ components/home-display.tsx
@@ -1,13 +1,17 @@
 import { TypographyH1 } from "@/components/typography/typography-h1";
 import { TypographyP } from "./typography/typography-p";
 import TypographyH3 from "./typography/typography-h3";
+import ThreeScene from "./three/ThreeScene";
+import PenCanvas from "./three/pen-canvas";
 
 export function HomeDisplay() {
     return (
         <div className="custom-container w-full flex flex-col items-center justify-center">
-            <TypographyH1 text={"Blog Builder"} />
-            <TypographyH3 text={"Craft your own blogs for free."} />
-            <TypographyP text={"Built with ❤️ by MetalFace"} />
+            <PenCanvas />
+            {/* <ThreeScene /> */}
+            <TypographyH1 text={"the pen is mightier than the sword."} />
+            {/* <TypographyH3 text={"Craft your own blogs for free."} /> */}
+            {/* <TypographyP text={"Built with ❤️ by MetalFace"} /> */}
         </div>
     );
 }
