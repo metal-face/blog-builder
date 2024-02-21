e47ca9c80823088fdcb63fdb1d5bca35b77a70e8
metal-face <hughesbryan3000@gmail.com>
2024-02-21 12:51:24 -0500

    adjusted to use session id as per @hacksore

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 type PostData = {
     blogTitle: string;
@@ -7,19 +9,32 @@ type PostData = {
     userId: string;
 };
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
+    const session = await getServerSession(authOptions);
     const { blogTitle, blogPost, userId } = req.body as PostData;
     const updatedAt = new Date();
 
+    console.log(session);
+
     try {
+        // @ts-ignore
+        if (!session?.user?.id) {
+            return new Response("Not authorized", { status: 401 });
+        }
         const newPost = await prisma.blogPosts.create({
             data: {
                 blogTitle,
                 blogPost,
-                userId,
-                updatedAt
+                // @ts-ignore
+                userId: session?.user.id,
+                updatedAt,
             },
         });
-        res.status(200).json(newPost);
+        return new Response(JSON.stringify({}), {
+            status: 200,
+            headers: {
+                "content-type": "application/json",
+            },
+        });
     } catch (error) {
         console.error("Request error", error);
         res.status(500).json({ error: "Error creating post" });
