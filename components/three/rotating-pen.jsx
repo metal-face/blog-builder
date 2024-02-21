b689bc48ced716204da64a6fff8af9e9dae0e9a5
metal-face <hughesbryan3000@gmail.com>
2024-02-21 12:52:21 -0500

    sauce for rotating 3d pen added

---
+++ components/three/rotating-pen.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useGLTF } from "@react-three/drei";
+
+export default function RotatingPen(props) {
+  const { nodes, materials } = useGLTF("/Fountain_Pen.gltf");
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Body_body_pen01.geometry}
+        material={materials.glossy_black}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Body_body_pen_part_01.geometry}
+        material={materials.gold}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Body_body_pen_part_02.geometry}
+        material={materials.chrome}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Body_body_pen_part_04.geometry}
+        material={materials.mat_black}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Top_Object01.geometry}
+        material={materials.gold}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Top_Object03.geometry}
+        material={materials.glossy_black}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/Fountain_Pen.gltf");
