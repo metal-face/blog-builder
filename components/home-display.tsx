806ee28dd03b158b48bc8dea1d6e4bd5c3c10310
metal-face <hughesbryan3000@gmail.com>
2024-02-24 06:41:59 -0500

    Refactor home-display component

--- components/home-display.tsx
+++ components/home-display.tsx
@@ -1,17 +1,11 @@
-import { TypographyH1 } from "@/components/typography/typography-h1";
-import { TypographyP } from "./typography/typography-p";
-import TypographyH3 from "./typography/typography-h3";
-import ThreeScene from "./three/ThreeScene";
-import PenCanvas from "./three/pen-canvas";
+import PenCanvas from "@/components/three/pen-canvas";
+import { TypographyH2 } from "@/components/typography/typography-h2";
 
 export function HomeDisplay() {
     return (
         <div className="custom-container w-full flex flex-col items-center justify-center">
             <PenCanvas />
-            {/* <ThreeScene /> */}
-            <TypographyH1 text={"the pen is mightier than the sword."} />
-            {/* <TypographyH3 text={"Craft your own blogs for free."} /> */}
-            {/* <TypographyP text={"Built with ❤️ by MetalFace"} /> */}
+            <TypographyH2 text={"The Pen Is Mightier Than The Sword"} />
         </div>
     );
 }
