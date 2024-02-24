b0834f69abf1127e4daa7b13108a099a0ea304c6
metal-face <hughesbryan3000@gmail.com>
2024-02-24 06:42:10 -0500

    Update pen-canvas and typography-h2 components

--- components/three/pen-canvas.tsx
+++ components/three/pen-canvas.tsx
@@ -4,6 +4,7 @@ import { Canvas } from "@react-three/fiber";
 import { FC, Suspense, useRef } from "react";
 import { OrbitControls, Stage } from "@react-three/drei";
 import { RotatingPen } from "@/components/three/rotating-pen";
+import { Camera } from "three";
 
 const PenCanvas = () => {
     const ref = useRef();
@@ -14,19 +15,24 @@ const PenCanvas = () => {
                 gl={{ preserveDrawingBuffer: true }}
                 shadows
                 dpr={[1, 1.5]}
-                camera={{ position: [0, 0, 150], fov: 50 }}
+                camera={{
+                    position: [0, 0, 150],
+                    near: 0.1,
+                    far: 1000,
+                    fov: 50,
+                }}
             >
                 <Suspense fallback={null}>
-                    <Stage
-                        intensity={10}
-                        castShadow={true}
-                        shadows
-                        adjustCamera
-                    >
+                    <Stage intensity={15} castShadow shadows adjustCamera>
                         <RotatingPen />
                     </Stage>
                 </Suspense>
-                <OrbitControls ref={ref} autoRotate={true} />
+                <OrbitControls
+                    ref={ref.current}
+                    autoRotate
+                    enableZoom={false}
+                    autoRotateSpeed={1.5}
+                />
             </Canvas>
         </div>
     );
