03f2df344b1ee53a528cd09e1de4ecc393adbb4c
metal-face <hughesbryan3000@gmail.com>
2024-02-24 06:42:17 -0500

    Refactor typography-h2 component

--- components/typography/typography-h2.tsx
+++ components/typography/typography-h2.tsx
@@ -1,10 +1,10 @@
 interface TypographyH2Props {
-  text: string;
+    text: string;
 }
 export function TypographyH2({ text }: TypographyH2Props) {
-  return (
-    <h2 className="scroll-m-20 border-b text-3xl font-semibold tracking-tight first:mt-0">
-      {text}
-    </h2>
-  );
+    return (
+        <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
+            {text}
+        </h2>
+    );
 }
