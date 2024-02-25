c923663602108bc30400bc104b3c11eb3b3ce4b2
metal-face <hughesbryan3000@gmail.com>
2024-02-25 02:48:47 -0500

    text alignment added to dropdown

--- components/editor/text-dropdown.tsx
+++ components/editor/text-dropdown.tsx
@@ -5,6 +5,8 @@ import {
     DropdownMenuCheckboxItem,
     DropdownMenuTrigger,
     DropdownMenuShortcut,
+    DropdownMenuSeparator,
+    DropdownMenuLabel,
 } from "@/components/ui/dropdown-menu";
 import {
     ChevronDown,
@@ -15,6 +17,9 @@ import {
     List,
     ListOrdered,
     Minus,
+    AlignLeft,
+    AlignCenter,
+    AlignRight,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
@@ -32,7 +37,12 @@ export default function TextDropdown({ editor }: Props) {
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel className="text-xs">
+                    Elements
+                </DropdownMenuLabel>
+                {/* HEADING 1 */}
                 <DropdownMenuCheckboxItem
+                    className="text-xs"
                     checked={editor.isActive("heading", { level: 1 })}
                     onCheckedChange={() =>
                         editor.chain().focus().toggleHeading({ level: 1 }).run()
@@ -43,7 +53,9 @@ export default function TextDropdown({ editor }: Props) {
                         <Heading1 />
                     </DropdownMenuShortcut>
                 </DropdownMenuCheckboxItem>
+                {/* HEADING 2 */}
                 <DropdownMenuCheckboxItem
+                    className="text-xs"
                     checked={editor.isActive("heading", { level: 2 })}
                     onCheckedChange={() =>
                         editor.chain().focus().toggleHeading({ level: 2 }).run()
@@ -54,7 +66,9 @@ export default function TextDropdown({ editor }: Props) {
                         <Heading2 />
                     </DropdownMenuShortcut>
                 </DropdownMenuCheckboxItem>
+                {/* HEADING 3 */}
                 <DropdownMenuCheckboxItem
+                    className="text-xs"
                     checked={editor.isActive("heading", { level: 3 })}
                     onCheckedChange={() =>
                         editor.chain().focus().toggleHeading({ level: 3 }).run()
@@ -65,7 +79,9 @@ export default function TextDropdown({ editor }: Props) {
                         <Heading3 />
                     </DropdownMenuShortcut>
                 </DropdownMenuCheckboxItem>
+                {/* CHECKLIST */}
                 <DropdownMenuCheckboxItem
+                    className="text-xs"
                     checked={editor.isActive("taskList")}
                     onCheckedChange={() =>
                         editor.chain().focus().toggleTaskList().run()
@@ -76,7 +92,9 @@ export default function TextDropdown({ editor }: Props) {
                         <ListTodo />
                     </DropdownMenuShortcut>
                 </DropdownMenuCheckboxItem>
+                {/* BULLET LIST */}
                 <DropdownMenuCheckboxItem
+                    className="text-xs"
                     checked={editor.isActive("bulletList")}
                     onCheckedChange={() =>
                         editor.chain().focus().toggleBulletList().run()
@@ -87,7 +105,9 @@ export default function TextDropdown({ editor }: Props) {
                         <List />
                     </DropdownMenuShortcut>
                 </DropdownMenuCheckboxItem>
+                {/* NUMBERED LIST */}
                 <DropdownMenuCheckboxItem
+                    className="text-xs"
                     checked={editor.isActive("orderedList")}
                     onCheckedChange={() =>
                         editor.chain().focus().toggleOrderedList().run()
@@ -98,7 +118,9 @@ export default function TextDropdown({ editor }: Props) {
                         <ListOrdered />
                     </DropdownMenuShortcut>
                 </DropdownMenuCheckboxItem>
+                {/* HORIZONTAL LINE */}
                 <DropdownMenuCheckboxItem
+                    className="text-xs"
                     checked={editor.isActive("horizontalRule")}
                     onCheckedChange={() =>
                         editor.chain().focus().setHorizontalRule().run()
@@ -107,6 +129,51 @@ export default function TextDropdown({ editor }: Props) {
                     Horizontal Line
                     <DropdownMenuShortcut>
                         <Minus />
+                    </DropdownMenuShortcut>
+                </DropdownMenuCheckboxItem>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuLabel className="text-xs">
+                    Positioning
+                </DropdownMenuLabel>
+                {/* LEFT ALIGN */}
+                <DropdownMenuCheckboxItem
+                    className="text-xs"
+                    checked={editor.isActive({ textAlign: "left" })}
+                    onCheckedChange={() => {
+                        editor.chain().focus().setTextAlign("left").run();
+                    }}
+                >
+                    Left
+                    <DropdownMenuShortcut>
+                        <AlignLeft />
+                    </DropdownMenuShortcut>
+                </DropdownMenuCheckboxItem>
+                {/* CENTER ALIGN */}
+                <DropdownMenuCheckboxItem
+                    className="text-xs"
+                    checked={editor.isActive({ textAlign: "center" })}
+                    onCheckedChange={() => {
+                        editor.chain().focus().setTextAlign("center").run();
+                    }}
+                >
+                    Center
+                    <DropdownMenuShortcut>
+                        <AlignCenter />
+                    </DropdownMenuShortcut>
+                </DropdownMenuCheckboxItem>
+                {/* RIGHT ALIGN */}
+                <DropdownMenuCheckboxItem
+                    className="text-xs"
+                    checked={editor.isActive({ textAlign: "right" })}
+                    onCheckedChange={() => {
+                        editor.chain().focus().setTextAlign("right").run();
+                    }}
+                >
+                    Right
+                    <DropdownMenuShortcut>
+                        <AlignRight />
                     </DropdownMenuShortcut>
                 </DropdownMenuCheckboxItem>
             </DropdownMenuContent>
