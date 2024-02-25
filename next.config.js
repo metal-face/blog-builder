c7c457c39b93eaa531310d08f7ed12d193c384b5
metal-face <hughesbryan3000@gmail.com>
2024-02-25 02:27:29 -0500

    ensured three packages are transpiled

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-const removeImports = require('next-remove-imports')();
+const nextConfig = {
+    transpilePackages: ["three"],
+};
+const removeImports = require("next-remove-imports")();
 module.exports = removeImports({});
