caf9e51181ff03365fffe668d717039331af9d52
metal-face <hughesbryan3000@gmail.com>
2024-02-25 02:28:27 -0500

    home message adjusted

--- components/home-display.tsx
+++ components/home-display.tsx
@@ -5,7 +5,7 @@ export function HomeDisplay() {
     return (
         <div className="custom-container w-full flex flex-col items-center justify-center">
             <PenCanvas />
-            <TypographyH2 text={"The Pen Is Mightier Than The Sword"} />
+            <TypographyH2 text={"The pen is mightier than the sword."} />
         </div>
     );
 }
