4d3699adbaa3dd51959b262e02239859265afc66
metal-face <hughesbryan3000@gmail.com>
2024-04-07 00:56:30 -0400

    zustand store defined

---
+++ lib/zustand.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+type BlogStore = {
+    blogTitle: string;
+    blogPost: string;
+};
+
+type BlogStoreActions = {
+    updateBlogTitle: (blogTitle: BlogStore["blogTitle"]) => void;
+    updateBlogPost: (blogPost: BlogStore["blogPost"]) => void;
+};
+
+export const useStore = create<BlogStore & BlogStoreActions>()(
+    persist(
+        (set) => ({
+            blogTitle: "",
+            blogPost: "",
+            updateBlogTitle: (blogTitle: string) => set({ blogTitle }),
+            updateBlogPost: (blogPost: string) => set({ blogPost }),
+        }),
+        {
+            name: "blog-store",
+            storage: createJSONStorage(() => sessionStorage),
+        }
+    )
+);
