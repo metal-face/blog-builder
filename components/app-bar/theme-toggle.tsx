485168308394c55ac55da2b8880eb205726cd570
metal-face <hughesbryan3000@gmail.com>
2024-06-01 23:20:27 -0400

    theme toggle refactored

--- components/app-bar/theme-toggle.tsx
+++ components/app-bar/theme-toggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Sun, Moon } from "lucide-react";
+import { Sun, Moon, Cpu } from "lucide-react";
 
 import { useTheme } from "next-themes";
 
@@ -9,7 +9,9 @@ import { Button } from "@/components/ui/button";
 import {
     DropdownMenu,
     DropdownMenuContent,
+    DropdownMenuGroup,
     DropdownMenuItem,
+    DropdownMenuShortcut,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
@@ -19,30 +21,41 @@ export function ThemeToggle() {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                    <Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Button variant="outline" size="sm">
+                    <Sun className="scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                     <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-                <DropdownMenuItem
-                    className="cursor-pointer"
-                    onClick={() => setTheme("light")}
-                >
-                    Light
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                    className="cursor-pointer"
-                    onClick={() => setTheme("dark")}
-                >
-                    Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                    className="cursor-pointer"
-                    onClick={() => setTheme("system")}
-                >
-                    System
-                </DropdownMenuItem>
+                <DropdownMenuGroup>
+                    <DropdownMenuItem
+                        className="cursor-pointer"
+                        onClick={() => setTheme("light")}
+                    >
+                        Light
+                        <DropdownMenuShortcut>
+                            <Sun />
+                        </DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        className="cursor-pointer"
+                        onClick={() => setTheme("dark")}
+                    >
+                        Dark
+                        <DropdownMenuShortcut>
+                            <Moon />
+                        </DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        className="cursor-pointer"
+                        onClick={() => setTheme("system")}
+                    >
+                        System
+                        <DropdownMenuShortcut>
+                            <Cpu />
+                        </DropdownMenuShortcut>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
             </DropdownMenuContent>
         </DropdownMenu>
     );
