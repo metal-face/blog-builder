574f2a5e0aea9b9783e76a9d20b8f62f8f45c3de
metal-face <hughesbryan3000@gmail.com>
2024-06-02 00:21:44 -0400

    variants enum typed

---
+++ models/variants.ts
@@ -0,0 +1,8 @@
+export enum Variants {
+    DEFAULT = "default",
+    DESTRUCTIVE = "destructive",
+    OUTLINE = "outline",
+    SECONDARY = "secondary",
+    GHOST = "ghost",
+    LINK = "link",
+}
