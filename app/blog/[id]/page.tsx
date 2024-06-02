40c7332ddff94dc60435a537ff5e67368b3d6724
metal-face <hughesbryan3000@gmail.com>
2024-06-02 02:03:42 -0400

    basic blog post page stubbed out

---
+++ app/blog/[id]/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import prisma from "@/lib/prisma";
+import { TypographyH1 } from "@/components/typography/typography-h1";
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const blogPost = await prisma.blogPosts.findFirst({
+        where: { id: params.id },
+    });
+
+    console.log(blogPost);
+    return (
+        <div className="h-full w-full flex flex-col justify-center">
+            <div className="m-4 text-center">
+                <TypographyH1
+                    text={blogPost?.blogTitle ? blogPost.blogTitle : ""}
+                />
+            </div>
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: blogPost?.blogPost ? blogPost.blogPost : "",
+                }}
+            />
+        </div>
+    );
+}
