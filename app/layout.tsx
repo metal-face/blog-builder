64af5875f7be58d973d4697a41d3c7389100b53f
metal-face <hughesbryan3000@gmail.com>
2024-07-05 02:18:25 -0400

    Layout adjusted

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,34 @@
-import React from "react";
 import { Inter } from "next/font/google";
 import { Providers } from "@/components/providers";
 import { MainNav } from "@/components/app-bar/main-nav";
+import { Toaster } from "@/components/ui/toaster";
+import { NextFont } from "next/dist/compiled/@next/font";
+import React, { ReactNode } from "react";
 import "./globals.css";
 import "@/app/login/style.css";
-import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter: NextFont = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html suppressHydrationWarning lang="en">
             <head>
                 <meta charSet="UTF-8" />
-                <meta
-                    name="keywords"
-                    content="Blog, Blogging, blog, blogging, writing"
-                />
+                <meta name="keywords" content="Blog, Blogging, blog, blogging, writing" />
                 <meta name="author" content="metal-face" />
                 <meta name="darkreader-lock" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
                 <title>Blog Builder</title>
             </head>
             <body className={inter.className + " overflow-x-hidden"}>
-                <Providers>
-                    <main className="h-screen w-screen">
+                <main className="h-screen w-screen">
+                    <Providers>
                         <MainNav />
                         {children}
-                    </main>
-                    <Toaster />
-                </Providers>
+                    </Providers>
+                </main>
+                <Toaster />
             </body>
         </html>
     );
