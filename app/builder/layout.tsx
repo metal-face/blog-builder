69bcefd3f842dcdb1772676846469a75259bdbb9
metal-face <hughesbryan3000@gmail.com>
2024-07-05 02:18:15 -0400

    prettier formatting, stricter typing

--- app/builder/layout.tsx
+++ app/builder/layout.tsx
@@ -1,13 +1,10 @@
 import { auth } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
+import { Session } from "next-auth";
 
-export default async function BuilderLayout({
-    children,
-}: {
-    children: ReactNode;
-}) {
-    const session = await auth();
+export default async function BuilderLayout({ children }: { children: ReactNode }) {
+    const session: Session | null = await auth();
 
     if (!session) {
         return redirect("/login");
