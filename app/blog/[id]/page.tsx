c08434c4cfa857024a3ebb49a5c05b9dca4593ae
metal-face <hughesbryan3000@gmail.com>
2024-07-05 02:18:57 -0400

    prettier formatting applied

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -7,13 +7,10 @@ export default async function Page({ params }: { params: { id: string } }) {
         where: { id: params.id },
     });
 
-    console.log(blogPost);
     return (
         <div className="h-full w-full flex flex-col justify-center">
             <div className="m-4 text-center">
-                <TypographyH1
-                    text={blogPost?.blogTitle ? blogPost.blogTitle : ""}
-                />
+                <TypographyH1 text={blogPost?.blogTitle ? blogPost.blogTitle : ""} />
             </div>
             <div
                 className="w-4/5 mx-auto outline p-4 rounded"
