39dae234b860626b588d914f865d5c8c16d18cbe
metal-face <hughesbryan3000@gmail.com>
2024-07-05 02:48:40 -0400

    blog posts typed

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -2,19 +2,19 @@ import { auth } from "@/app/api/auth/[...nextauth]/route";
 import { format } from "date-fns";
 import { TypographyH1 } from "@/components/typography/typography-h1";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import React from "react";
-import prisma from "@/lib/prisma";
+import { Session } from "next-auth";
+import React, { ReactElement } from "react";
+import { prisma, BlogPosts } from "@/lib/prisma";
 import Link from "next/link";
 
-export default async function Page() {
-    const session = await auth();
+export default async function Page(): Promise<ReactElement> {
+    const session: Session | null = await auth();
 
-    const blogs = await prisma?.blogPosts.findMany({
+    const blogs: BlogPosts[] = await prisma.blogPosts.findMany({
         where: { userId: session?.user.id },
-        select: { blogTitle: true, createdAt: true, id: true },
     });
 
-    const blogCard = blogs.map((blog) => (
+    const blogCard: ReactElement[] = blogs.map((blog: BlogPosts) => (
         <div key={blog.id}>
             <Link href={`/blog/${blog.id}`}>
                 <Card className="hover:outline-1">
