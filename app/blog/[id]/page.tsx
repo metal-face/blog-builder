b4931a6683b25efcfdca22336934cceeb4ecf30e
metal-face <hughesbryan3000@gmail.com>
2024-07-07 23:38:18 -0400

    blog card moved to client side in own component

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import prisma from "@/lib/prisma";
+import { prisma, BlogPosts } from "@/lib/prisma";
 import { TypographyH1 } from "@/components/typography/typography-h1";
 
 export default async function Page({ params }: { params: { id: string } }) {
-    const blogPost = await prisma.blogPosts.findFirst({
+    const blogPost: BlogPosts | null = await prisma.blogPosts.findFirst({
         where: { id: params.id },
     });
 
     return (
-        <div className="h-full w-full flex flex-col justify-center">
+        <div className="h-4/5 w-full flex flex-col">
             <div className="m-4 text-center">
                 <TypographyH1 text={blogPost?.blogTitle ? blogPost.blogTitle : ""} />
             </div>
             <div
-                className="w-4/5 mx-auto outline p-4 rounded"
+                className="w-4/5 h-full overflow-y-auto mx-auto outline p-4 rounded"
                 dangerouslySetInnerHTML={{
                     __html: blogPost?.blogPost ? blogPost.blogPost : "",
                 }}
