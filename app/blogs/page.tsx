7fb50d9cc0e8a8f51c7c36577a97d2547000ec21
metal-face <hughesbryan3000@gmail.com>
2024-07-08 02:51:47 -0400

    only non-deleted blogs fetched

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,19 +1,20 @@
 import { auth } from "@/app/api/auth/[...nextauth]/route";
 import { TypographyH1 } from "@/components/typography/typography-h1";
 import { Session } from "next-auth";
+import { BlogPosts } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 import React, { ReactElement } from "react";
-import { prisma, BlogPosts } from "@/lib/prisma";
 import BlogCards from "@/components/blogs/blog-cards";
 
 export default async function Page(): Promise<ReactElement> {
     const session: Session | null = await auth();
 
     const blogs: BlogPosts[] = await prisma.blogPosts.findMany({
-        where: { userId: session?.user.id },
+        where: { userId: session?.user.id, deletedAt: null },
     });
 
     return (
-        <div className="h-full w-full flex flex-col items-center">
+        <div className="h-full w-full sm:w-5/6 lg:w-4/5 mx-auto flex flex-col items-center">
             <div className="text-center m-3">
                 <TypographyH1 text="My Blogs" />
             </div>
