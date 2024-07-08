6d73993454af7b83de0721795f009bb2f062a0b5
metal-face <hughesbryan3000@gmail.com>
2024-07-07 23:38:29 -0400

    blog card moved to client side in own component

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,11 +1,9 @@
 import { auth } from "@/app/api/auth/[...nextauth]/route";
-import { format } from "date-fns";
 import { TypographyH1 } from "@/components/typography/typography-h1";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Session } from "next-auth";
 import React, { ReactElement } from "react";
 import { prisma, BlogPosts } from "@/lib/prisma";
-import Link from "next/link";
+import BlogCards from "@/components/blogs/blog-cards";
 
 export default async function Page(): Promise<ReactElement> {
     const session: Session | null = await auth();
@@ -14,34 +12,14 @@ export default async function Page(): Promise<ReactElement> {
         where: { userId: session?.user.id },
     });
 
-    const blogCard: ReactElement[] = blogs.map((blog: BlogPosts) => (
-        <div
-            key={blog.id}
-            className={
-                "drop-shadow-3xl m-2 hover:scale-110 transform-gpu transition-transform duration-300"
-            }
-        >
-            <Link href={`/blog/${blog.id}`}>
-                <Card className="hover:outline-1">
-                    <CardHeader>
-                        <CardTitle className="text-center hover:underline">
-                            {blog.blogTitle}
-                        </CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <p className="text-center text-sm">{format(blog.createdAt, "PPPP")}</p>
-                    </CardContent>
-                </Card>
-            </Link>
-        </div>
-    ));
-
     return (
         <div className="h-full w-full flex flex-col items-center">
             <div className="text-center m-3">
                 <TypographyH1 text="My Blogs" />
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">{blogCard}</div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full">
+                <BlogCards initData={blogs} />
+            </div>
         </div>
     );
 }
