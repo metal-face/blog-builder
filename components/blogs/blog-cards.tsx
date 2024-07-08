a7a79e36287d086a748737ed63ee7957eb597536
metal-face <hughesbryan3000@gmail.com>
2024-07-07 23:35:17 -0400

    Component that creates and displays all blog cards written

---
+++ components/blogs/blog-cards.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import React, { ReactElement, useEffect, useState } from "react";
+import { BlogPosts } from "@prisma/client";
+import BlogCard from "@/components/blogs/blog-card";
+import ResponsiveDialog from "@/components/responsive-dialog";
+
+interface Props {
+    initData: BlogPosts[];
+}
+
+export default function BlogCards({ initData }: Props) {
+    const [fetchData, setFetchData] = useState<boolean>(false);
+    const [triggerDelete, setTriggerDelete] = useState<boolean>(false);
+    const [dialogVisibility, setDialogVisibility] = useState<boolean>(false);
+    const [blogIdToDelete, setBlogIdToDelete] = useState<string>("");
+
+    const initBlogCards: ReactElement[] = initData.map((blog: BlogPosts) => (
+        <BlogCard
+            key={blog.id}
+            blog={blog}
+            blogIdToDelete={blogIdToDelete}
+            setFetchData={setFetchData}
+            triggerDelete={triggerDelete}
+            setDialogVisibility={setDialogVisibility}
+            setBlogIdToDelete={setBlogIdToDelete}
+        />
+    ));
+
+    const [data, setData] = useState<ReactElement[]>(initBlogCards);
+
+    useEffect(() => {
+        const fetchAllBlogs = async () => {
+            const res = await fetch("/api/blogs", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (res.ok) {
+                const payload = await res.json();
+
+                const newBlogPosts: BlogPosts[] = payload as BlogPosts[];
+
+                const transformed: ReactElement[] = newBlogPosts.map((blog: BlogPosts) => (
+                    <BlogCard
+                        key={blog.id}
+                        blog={blog}
+                        blogIdToDelete={blogIdToDelete}
+                        setFetchData={setFetchData}
+                        triggerDelete={triggerDelete}
+                        setDialogVisibility={setDialogVisibility}
+                        setBlogIdToDelete={setBlogIdToDelete}
+                    />
+                ));
+
+                setData(transformed);
+            }
+        };
+
+        fetchAllBlogs();
+    }, [fetchData, triggerDelete, blogIdToDelete]);
+
+    return (
+        <>
+            <ResponsiveDialog
+                visible={dialogVisibility}
+                setVisibility={setDialogVisibility}
+                setTriggerDelete={setTriggerDelete}
+            />
+            {data}
+        </>
+    );
+}
