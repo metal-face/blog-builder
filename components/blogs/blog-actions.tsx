dd8ddd9bef20a5a2e4d464cfc865fb91b9590f75
metal-face <hughesbryan3000@gmail.com>
2024-07-07 23:34:44 -0400

    Component for blog card actions written

---
+++ components/blogs/blog-actions.tsx
@@ -0,0 +1,45 @@
+import React, { Dispatch, SetStateAction, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Pencil, TrashIcon } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+
+interface Props {
+    blogId: string;
+    setDialogVisibility: Dispatch<SetStateAction<boolean>>;
+    setBlogIdToDelete: Dispatch<SetStateAction<string>>;
+}
+
+export default function BlogActions({ blogId, setDialogVisibility, setBlogIdToDelete }: Props) {
+    return (
+        <div className={"gap-2 flex w-full items-center justify-end "}>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Button
+                        onClick={() => {
+                            setBlogIdToDelete(blogId);
+                            setDialogVisibility(true);
+                        }}
+                        size={"icon"}
+                        variant={"ghost"}
+                    >
+                        <TrashIcon />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <span>Delete</span>
+                </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Button size={"icon"} variant={"ghost"}>
+                        <Pencil />
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <span>Update</span>
+                </TooltipContent>
+            </Tooltip>
+        </div>
+    );
+}
