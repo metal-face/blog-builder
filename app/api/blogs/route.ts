14ad1dfdfcb6d290b466338015e91f306ec5bf74
metal-face <hughesbryan3000@gmail.com>
2024-07-07 23:38:54 -0400

    Delete and get many routes written

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,16 +1,22 @@
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { auth } from "@/app/api/auth/[...nextauth]/route";
 import { Session } from "next-auth";
+import { BlogPosts } from "@prisma/client";
+import { NextRequest } from "next/server";
 
-type PostData = {
+interface PostData {
     blogTitle: string;
     blogPost: string;
-};
+}
+
+interface DeleteData {
+    blogId: string;
+}
 
 export async function POST(req: Request): Promise<Response> {
     const session: Session | null = await auth();
-    const { blogTitle, blogPost }: PostData = (await req.json()) as PostData;
     const updatedAt: Date = new Date();
+    const { blogTitle, blogPost }: PostData = (await req.json()) as PostData;
 
     try {
         if (!session || !session.user.id) {
@@ -33,6 +39,54 @@ export async function POST(req: Request): Promise<Response> {
         return Response.json({}, { status: 400, statusText: "Bad Request" });
     } catch (error) {
         console.error("Request error", error);
+        return Response.json({}, { status: 500, statusText: "Internal Server Error" });
+    }
+}
+
+export async function DELETE(req: Request): Promise<Response> {
+    const session = await auth();
+    const payload: DeleteData = (await req.json()) as DeleteData;
+    const blogId = payload.blogId;
+
+    if (!blogId || !session) {
+        return Response.json({}, { status: 400, statusText: "Bad Request" });
+    }
+
+    try {
+        const res = await prisma.blogPosts.deleteMany({
+            where: { id: blogId, userId: session.user.id },
+        });
+
+        if (res) {
+            return Response.json({}, { status: 200, statusText: "Success" });
+        }
+
+        return Response.json({}, { status: 404, statusText: "Not Found" });
+    } catch (err: any) {
+        return Response.json({}, { status: 500, statusText: "Internal Server Error" });
+    }
+}
+
+export async function GET(req: NextRequest): Promise<Response> {
+    const session: Session | null = await auth();
+
+    if (!session) {
+        return Response.json({}, { status: 400, statusText: "Bad Request" });
+    }
+
+    try {
+        const posts: BlogPosts[] = await prisma.blogPosts.findMany({
+            where: {
+                userId: session.user.id,
+            },
+        });
+
+        if (!posts) {
+            return Response.json({}, { status: 404, statusText: "Not Found" });
+        }
+
+        return Response.json(posts);
+    } catch (err: any) {
         return Response.json({}, { status: 500, statusText: "Internal Server Error" });
     }
 }
