c3a48abe4982ae64160e9257a7f4a1fb5a4d64be
metal-face <hughesbryan3000@gmail.com>
2024-07-09 23:16:31 -0400

    bug fix, if not logged in, redirect to login

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -3,17 +3,26 @@ import { TypographyH1 } from "@/components/typography/typography-h1";
 import { Session } from "next-auth";
 import { BlogPosts } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
-import React, { ReactElement } from "react";
-import BlogCards from "@/components/blogs/blog-cards";
 import { TypographyP } from "@/components/typography/typography-p";
 import { Button } from "@/components/ui/button";
 import { Hammer } from "lucide-react";
+import { redirect } from "next/navigation";
+import React, { ReactElement } from "react";
+import BlogCards from "@/components/blogs/blog-cards";
 import Link from "next/link";
 
 export default async function Page(): Promise<ReactElement> {
     const session: Session | null = await auth();
 
     if (!session) {
+        redirect("/login");
+    }
+
+    const blogs: BlogPosts[] = await prisma.blogPosts.findMany({
+        where: { userId: session.user.id, deletedAt: null },
+    });
+
+    if (blogs.length === 0) {
         return (
             <div className="h-full w-full sm:w-5/6 lg:w-4/5 mx-auto flex flex-col items-center">
                 <div className="text-center m-3">
@@ -30,10 +39,6 @@ export default async function Page(): Promise<ReactElement> {
             </div>
         );
     }
-
-    const blogs: BlogPosts[] = await prisma.blogPosts.findMany({
-        where: { userId: session.user.id, deletedAt: null },
-    });
 
     return (
         <div className="h-full w-full sm:w-5/6 lg:w-4/5 mx-auto flex flex-col items-center">
