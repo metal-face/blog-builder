034a07c2ac926c3ae2c4c51568b33cc1d6cb8a1b
metal-face <hughesbryan3000@gmail.com>
2024-07-12 14:31:19 -0400

    strict mode added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ["three"],
+    reactStrictMode: true,
 };
 const removeImports = require("next-remove-imports")();
 module.exports = removeImports({});
