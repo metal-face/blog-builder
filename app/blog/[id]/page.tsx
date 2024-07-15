4fc3dade253fb387501fe6ef96cb2bf76add5405
metal-face <hughesbryan3000@gmail.com>
2024-07-15 12:48:24 -0400

    TipTap editor used in Read-Only for viewing of individual post

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,23 +1,28 @@
-import React from "react";
 import { prisma, BlogPosts } from "@/lib/prisma";
 import { TypographyH1 } from "@/components/typography/typography-h1";
+import Tiptap from "@/components/editor/tip-tap";
 
 export default async function Page({ params }: { params: { id: string } }) {
-    const blogPost: BlogPosts | null = await prisma.blogPosts.findFirst({
-        where: { id: params.id },
-    });
+    try {
+        const blogPost: BlogPosts | null = await prisma.blogPosts.findFirst({
+            where: { id: params.id },
+        });
 
-    return (
-        <div className="h-4/5 w-full flex flex-col">
-            <div className="m-4 text-center">
-                <TypographyH1 text={blogPost?.blogTitle ? blogPost.blogTitle : ""} />
+        if (blogPost && blogPost.blogPost && blogPost.blogTitle) {
+            return (
+                <div className={"h-4/5 w-full flex justify-center flex-col"}>
+                    <div className={"text-center"}>
+                        <TypographyH1 text={blogPost.blogTitle} />
+                    </div>
+                    <Tiptap editable={false} blogPost={blogPost.blogPost} />
+                </div>
+            );
+        }
+    } catch {
+        return (
+            <div className="h-4/5 w-full flex justify-center items-center">
+                <TypographyH1 text={"Oops! We couldn't find your blog"} />
             </div>
-            <div
-                className="w-4/5 h-full overflow-y-auto mx-auto outline p-4 rounded"
-                dangerouslySetInnerHTML={{
-                    __html: blogPost?.blogPost ? blogPost.blogPost : "",
-                }}
-            />
-        </div>
-    );
+        );
+    }
 }
