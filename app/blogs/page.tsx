b370c9856b6b846b3fa2185a61734ea15609f7a6
metal-face <hughesbryan3000@gmail.com>
2024-07-30 04:20:16 -0400

    button to create new post added

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -5,11 +5,12 @@ import { BlogPosts } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
 import { TypographyP } from "@/components/typography/typography-p";
 import { Button } from "@/components/ui/button";
-import { Hammer } from "lucide-react";
+import { Hammer, Pencil } from "lucide-react";
 import { redirect } from "next/navigation";
 import React, { ReactElement } from "react";
 import BlogCards from "@/components/blogs/blog-cards";
 import Link from "next/link";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 
 export default async function Page(): Promise<ReactElement> {
     const session: Session | null = await auth();
@@ -41,13 +42,33 @@ export default async function Page(): Promise<ReactElement> {
     }
 
     return (
-        <div className="h-full w-full sm:w-5/6 lg:w-4/5 mx-auto flex flex-col items-center">
-            <div className="text-center m-3">
-                <TypographyH1 text="My Blogs" />
+        <>
+            <div className="h-full w-full sm:w-5/6 lg:w-4/5 mx-auto flex flex-col items-center">
+                <div className="text-center m-3">
+                    <TypographyH1 text="My Blogs" />
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full">
+                    <BlogCards initData={blogs} />
+                </div>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full">
-                <BlogCards initData={blogs} />
+            <div className={"sticky h-fit w-fit bottom-5 left-full mr-8"}>
+                <Link href={"/builder"}>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Button
+                                className={
+                                    "rounded-full h-24 w-24 bg-green-400 hover:bg-green-400 hover:rotate-[360deg] duration-500 ease-in-out transition-transform transform-gpu"
+                                }
+                            >
+                                <Pencil className={"w-8 h-8"} />
+                            </Button>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <span>New Post</span>
+                        </TooltipContent>
+                    </Tooltip>
+                </Link>
             </div>
-        </div>
+        </>
     );
 }
