30e4eaa809e049c65c76c67be8536685f9f78c7b
metal-face <hughesbryan3000@gmail.com>
2024-07-31 04:21:33 -0400

    testing if IP is present in production

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { prisma, BlogPosts } from "@/lib/prisma";
 import { TypographyH1 } from "@/components/typography/typography-h1";
 import Tiptap from "@/components/editor/tip-tap";
+import { NextRequest } from "next/server";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: { id: string } }, request: NextRequest) {
+    console.log("IP: ", request?.ip);
     try {
         const blogPost: BlogPosts | null = await prisma.blogPosts.findFirst({
             where: { id: params.id },
@@ -11,7 +13,7 @@ export default async function Page({ params }: { params: { id: string } }) {
         if (blogPost && blogPost.blogPost && blogPost.blogTitle) {
             return (
                 <div className={"h-4/5 w-full sm:w-11/12 mx-auto flex justify-center flex-col"}>
-                    <div className={"text-center"}>
+                    <div className={"text-center mb-2"}>
                         <TypographyH1 text={blogPost.blogTitle} />
                     </div>
                     <Tiptap editable={false} blogPost={blogPost.blogPost} />
