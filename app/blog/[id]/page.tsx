b582b77e51972a5e6f1f27d282c76e9b1f45ccc2
metal-face <hughesbryan3000@gmail.com>
2024-08-02 07:01:42 -0400

    logic added to count blog views by unique IP address

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,35 +1,70 @@
 import { prisma, BlogPosts } from "@/lib/prisma";
 import { TypographyH1 } from "@/components/typography/typography-h1";
+import { ReactElement } from "react";
 import Tiptap from "@/components/editor/tip-tap";
-import { NextRequest } from "next/server";
+import { ViewLog } from "@prisma/client";
+
+interface SearchParams {
+    ip: string | undefined;
+}
 
 export default async function Page({
     params,
     searchParams,
 }: {
     params: { id: string };
-    searchParams: { [key: string]: string | string[] | undefined };
-}) {
-    try {
-        const blogPost: BlogPosts | null = await prisma.blogPosts.findFirst({
-            where: { id: params.id },
+    searchParams: SearchParams;
+}): Promise<ReactElement> {
+    if (searchParams.ip && params.id) {
+        const hasViewed = await prisma.viewLog.findFirst({
+            where: {
+                postId: {
+                    contains: params.id,
+                },
+                ipAddress: {
+                    contains: searchParams.ip,
+                },
+            },
         });
 
-        if (blogPost && blogPost.blogPost && blogPost.blogTitle) {
-            return (
-                <div className={"h-4/5 w-full sm:w-11/12 mx-auto flex justify-center flex-col"}>
-                    <div className={"text-center mb-2"}>
-                        <TypographyH1 text={blogPost.blogTitle} />
-                    </div>
-                    <Tiptap editable={false} blogPost={blogPost.blogPost} />
-                </div>
-            );
+        if (!hasViewed) {
+            const viewLog: ViewLog = await prisma.viewLog.create({
+                data: {
+                    ipAddress: searchParams.ip,
+                    postId: params.id,
+                },
+            });
+
+            const updatedViewCount: BlogPosts = await prisma.blogPosts.update({
+                where: {
+                    id: params.id,
+                },
+                data: {
+                    pageViews: {
+                        increment: 1,
+                    },
+                },
+            });
         }
-    } catch {
+    }
+
+    const blogPost: BlogPosts | null = await prisma.blogPosts.findFirst({
+        where: { id: params.id },
+    });
+
+    if (blogPost && blogPost.blogPost && blogPost.blogTitle) {
         return (
-            <div className="h-4/5 w-full flex justify-center items-center">
-                <TypographyH1 text={"Oops! We couldn't find your blog"} />
+            <div className={"h-4/5 w-full sm:w-11/12 mx-auto flex justify-center flex-col"}>
+                <div className={"text-center mb-2"}>
+                    <TypographyH1 text={blogPost.blogTitle} />
+                </div>
+                <Tiptap editable={false} blogPost={blogPost.blogPost} />
             </div>
         );
     }
+    return (
+        <div className="h-4/5 w-full flex justify-center items-center">
+            <TypographyH1 text={"Oops! We couldn't find your blog"} />
+        </div>
+    );
 }
