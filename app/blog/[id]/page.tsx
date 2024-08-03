7dd58b4205b7e0b3c9b2726d4db69d3d6a6ff842
metal-face <hughesbryan3000@gmail.com>
2024-08-03 18:15:46 -0400

    session fetched for userId of viewer

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,8 +1,9 @@
 import { prisma, BlogPosts } from "@/lib/prisma";
 import { TypographyH1 } from "@/components/typography/typography-h1";
-import { ReactElement } from "react";
+import React, { ReactElement } from "react";
 import Tiptap from "@/components/editor/tip-tap";
-import { ViewLog } from "@prisma/client";
+import LikeButton from "@/components/blogs/like-button";
+import { auth } from "@/auth/auth";
 
 interface SearchParams {
     ip: string | undefined;
@@ -15,6 +16,7 @@ export default async function Page({
     params: { id: string };
     searchParams: SearchParams;
 }): Promise<ReactElement> {
+    const session = await auth();
     if (searchParams.ip && params.id) {
         const hasViewed = await prisma.viewLog.findFirst({
             where: {
@@ -28,23 +30,31 @@ export default async function Page({
         });
 
         if (!hasViewed) {
-            const viewLog: ViewLog = await prisma.viewLog.create({
-                data: {
-                    ipAddress: searchParams.ip,
-                    postId: params.id,
-                },
-            });
-
-            const updatedViewCount: BlogPosts = await prisma.blogPosts.update({
-                where: {
-                    id: params.id,
-                },
-                data: {
-                    pageViews: {
-                        increment: 1,
+            try {
+                await prisma.viewLog.create({
+                    data: {
+                        ipAddress: searchParams.ip,
+                        postId: params.id,
                     },
-                },
-            });
+                });
+            } catch (err: any) {
+                console.error(err);
+            }
+
+            try {
+                await prisma.blogPosts.update({
+                    where: {
+                        id: params.id,
+                    },
+                    data: {
+                        pageViews: {
+                            increment: 1,
+                        },
+                    },
+                });
+            } catch (err: any) {
+                console.error(err);
+            }
         }
     }
 
@@ -52,11 +62,32 @@ export default async function Page({
         where: { id: params.id },
     });
 
-    if (blogPost && blogPost.blogPost && blogPost.blogTitle) {
+    if (
+        blogPost &&
+        blogPost.blogPost &&
+        blogPost.blogTitle &&
+        searchParams.ip &&
+        session &&
+        session.user.id
+    ) {
+        const hasLiked = await prisma.likeLog.findFirst({
+            where: {
+                postId: params.id,
+                ipAddress: searchParams.ip,
+                userId: session.user.id,
+            },
+        });
         return (
             <div className={"h-4/5 w-full sm:w-11/12 mx-auto flex justify-center flex-col"}>
                 <div className={"text-center mb-2"}>
                     <TypographyH1 text={blogPost.blogTitle} />
+                    <LikeButton
+                        initialLikes={blogPost.likes}
+                        blogPostId={blogPost.id}
+                        ipAddress={searchParams.ip}
+                        userId={session?.user.id}
+                        hasLiked={!!hasLiked}
+                    />
                 </div>
                 <Tiptap editable={false} blogPost={blogPost.blogPost} />
             </div>
