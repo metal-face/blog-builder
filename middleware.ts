7a77772130af4bb8b702290ee67c0214a2bb499d
metal-face <hughesbryan3000@gmail.com>
2024-08-03 18:15:32 -0400

    matcher added

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const config = {
+    matcher: "/blog/:id*",
+};
+
 export async function middleware(request: NextRequest): Promise<NextResponse> {
     const ip: string = request.headers.get("x-forwarded-for") || "";
     const { nextUrl: url } = request;
