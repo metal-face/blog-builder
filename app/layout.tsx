ae6a9e983706498d8618f10ad198ff7ea6164c1f
metal-face <hughesbryan3000@gmail.com>
2024-08-08 12:24:35 -0400

    OG image fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,20 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                 <meta name="darkreader-lock" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta property="og:image" content="https://blog-builder.com/api/og" />
+
+                <meta property="og:url" content="https://blog-builder.com" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Blog Builder" />
+                <meta property="og:description" content="" />
+                <meta property="og:image" content="https://blog-builder.com/api/og" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="blog-builder.com" />
+                <meta property="twitter:url" content="https://blog-builder.com" />
+                <meta name="twitter:title" content="Blog Builder" />
+                <meta name="twitter:description" content="" />
+                <meta name="twitter:image" content="https://blog-builder.com/api/og" />
+
                 <link rel="icon" href="/icon.tsx" type="image/png" sizes="32x32" />
                 <title>Blog Builder</title>
             </head>
