a262149971256a931f396cd30269f0911a00e7f8
metal-face <hughesbryan3000@gmail.com>
2024-08-17 01:19:34 -0400

    create button refactored

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -7,10 +7,10 @@ import { TypographyP } from "@/components/typography/typography-p";
 import { Button } from "@/components/ui/button";
 import { Hammer, Pencil } from "lucide-react";
 import { redirect } from "next/navigation";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import React, { ReactElement } from "react";
 import BlogCards from "@/components/blogs/blog-cards";
 import Link from "next/link";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 
 export default async function Page(): Promise<ReactElement> {
     const session: Session | null = await auth();
@@ -47,27 +47,25 @@ export default async function Page(): Promise<ReactElement> {
                 <div className="text-center m-3">
                     <TypographyH1 text="My Blogs" />
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full gap-2">
                     <BlogCards initData={blogs} />
-                    <div className="rounded-lg border-2 border-dashed border-neutral-500/50 w-full m-auto h-[calc(100%-1rem)] bg-zinc-200/50 dark:bg-zinc-800/50 grid place-items-center">
-                        <div className={"h-fit w-fit m-auto bottom-8 right-8"}>
-                            <Link href={"/builder"}>
-                                <Tooltip>
-                                    <TooltipTrigger asChild>
-                                        <Button
-                                            className={
-                                                "rounded-full h-24 w-24 bg-green-500 dark:bg-green-400 hover:bg-green-400 dark:hover:bg-green-500 hover:rotate-[360deg] duration-500 ease-in-out transition-all transform-gpu"
-                                            }
-                                        >
-                                            <Pencil className={"w-8 h-8"} />
-                                        </Button>
-                                    </TooltipTrigger>
-                                    <TooltipContent>
-                                        <span>New Post</span>
-                                    </TooltipContent>
-                                </Tooltip>
-                            </Link>
-                        </div>
+                    <div className={"h-fit w-fit m-auto absolute bottom-8 right-8"}>
+                        <Link href={"/builder"}>
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Button
+                                        className={
+                                            "rounded-full m-2 h-24 w-24 bg-green-500 dark:bg-green-400 hover:bg-green-400 dark:hover:bg-green-500 hover:rotate-[360deg] duration-500 ease-in-out transition-all transform-gpu"
+                                        }
+                                    >
+                                        <Pencil className={"w-8 h-8"} />
+                                    </Button>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <span>New Post</span>
+                                </TooltipContent>
+                            </Tooltip>
+                        </Link>
                     </div>
                 </div>
             </div>
